79b016e6d8ef12201f1fa395370e0b6688bd74ad
Robert Christensen <rchristensen@wesleyan.edu>
2012-10-15 15:04:20 -0400

    Fixed a typo in VLC
    wat.

--- cookbooks/vlc/recipes/mac.rb
+++ cookbooks/vlc/recipes/mac.rb
@@ -8,7 +8,7 @@
 #
 dmg_package "vlc-2.0.3" do
   app "VLC"
-  volumes_dir "vlc-2.0.3""
+  volumes_dir "vlc-2.0.3"
   dmg_name "vlc-2.0.3"
   source "http://baratheon.class.wesleyan.edu/mac_os_x-10.8/vlc-2/vlc-2.0.3.dmg"
   checksum "d0ee6c536835c837c1043dde585fe9f36aa36661fe24b3e0662bcc468f9cf143"
