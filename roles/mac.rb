787dd0793fa1c46964e0b55852b00fc8b4fb93e5
Robert Christensen <rchristensen@wesleyan.edu>
2012-10-15 11:27:15 -0400

    Updated role for new packages
    Because I'm a dummy

--- roles/mac.rb
+++ roles/mac.rb
@@ -1,4 +1,4 @@
 name "mac"
 description "mac image"
-run_list "recipe[dmg]", "recipe[osxupdate::mac]", "recipe[java::mac]", "recipe[itunes::mac]"
-#run_list "recipe[dmg]", "recipe[chrome::mac]", "recipe[remotedesktop::mac]", "recipe[photoshop::mac]", "recipe[office::mac]"
+run_list "recipe[dmg]", "recipe[osxupdate::mac]", "recipe[java::mac]", "recipe[itunes::mac]", "recipe[unarchiver::mac]", "recipe[vlc::mac]"
+#"recipe[chrome::mac]", "recipe[remotedesktop::mac]", "recipe[photoshop::mac]", "recipe[office::mac]"
