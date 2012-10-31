2a6a72e0b9da40bf5ba7b84c37d01c0071f5f5dc
Robert Christensen <rchristensen@wesleyan.edu>
2012-10-31 16:48:33 -0400

    Updated finalcutpro cookbook
    App update!

--- cookbooks/finalcutpro/recipes/mac.rb
+++ cookbooks/finalcutpro/recipes/mac.rb
@@ -6,12 +6,12 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-dmg_package "finalcutpro-10.0.5.dmg" do
+dmg_package "finalcutpro-10.0.6.dmg" do
   app "Final Cut Pro"
   volumes_dir "Final Cut Pro"
-  dmg_name "finalcutpro-10.0.5"
-  source "http://baratheon.class.wesleyan.edu/mac_os_x-10.8/apple_final_cut_pro-10/finalcutpro-10.0.5.dmg"
-  checksum "36d4b90c3f405f07f577a7dd4f1b6826ba9797281e937b287a54f140e049b9c9"
+  dmg_name "finalcutpro-10.0.6"
+  source "http://baratheon.class.wesleyan.edu/mac_os_x-10.8/apple_final_cut_pro-10/finalcutpro-10.0.6.dmg"
+  checksum "67f9382847e77dcc753653c3f4d5360763128852a6e86c933a2566d601f77a38"
   action :install
   type "app"
 end
