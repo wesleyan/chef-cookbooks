0903966432ee3c65acc15871aa9ee1452367bdb1
Robert Christensen <rchristensen@wesleyan.edu>
2012-11-01 18:02:55 -0400

    Updated indesign to include dock icon

--- cookbooks/indesign/CHANGELOG.md
+++ cookbooks/indesign/CHANGELOG.md
@@ -2,11 +2,11 @@
 
 This file is used to list changes made in each version of indesign.
 
+## 0.2.0
+
+* Added dock icon
+* Current app version: 8.0.1
+
 ## 0.1.0:
 
 * Initial release of indesign cookbook
-
-- - - 
-Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
-
-The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
--- cookbooks/indesign/recipes/mac.rb
+++ cookbooks/indesign/recipes/mac.rb
@@ -6,6 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+# Install package
 dmg_package "adobe_indesign_cs6-8.0.1_Install.dmg" do
   app "adobe_indesign_cs6-8.0.1_Install"
   volumes_dir "adobe_indesign_cs6-8.0.1_Install"
@@ -15,4 +16,20 @@ dmg_package "adobe_indesign_cs6-8.0.1_Install.dmg" do
   action :install
   type "pkg"
   package_id "com.adobe.Enterprise.install.E5E7BB07-9B18-46F3-B829-E516CD1F9061"
+end
+
+# Create dock folder, if it does not exist
+dock_add "Adobe Creative Suite 6" do
+  all_users true
+  action :folder_create
+  show_as "list"
+  display_as "folder"
+  arrangement "name"
+end
+
+# Add icon to dock
+dock_add "/Applications/Adobe InDesign CS6/Adobe InDesign CS6.app" do
+  all_users true
+  group "Adobe Creative Suite 6"
+  restart true
 end
