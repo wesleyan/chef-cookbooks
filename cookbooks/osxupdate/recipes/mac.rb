266870ce4573fac523738e01811def66fe463bf5
Robert Christensen <rchristensen@wesleyan.edu>
2012-11-01 16:23:27 -0400

    Updated osxupdate to add specific 10.8.2 for new hardware, chrome/vlc dock

--- cookbooks/osxupdate/recipes/mac.rb
+++ cookbooks/osxupdate/recipes/mac.rb
@@ -1,4 +1,13 @@
-dmg_package "OSXUpdCombo10.8.2" do
+#
+# Cookbook Name:: osxupdate
+# Recipe:: mac
+#
+# Copyright 2012, Wesleyan University
+#
+# All rights reserved - Do Not Redistribute
+#
+# Install Combo Update
+dmg_package "OSXUpdCombo10.8.2.dmg" do
   app "OSXUpdCombo10.8.2"
   volumes_dir "OS X 10.8.2 Update Combo"
   dmg_name "OSXUpdCombo10.8.2"
@@ -7,4 +16,16 @@ dmg_package "OSXUpdCombo10.8.2" do
   action :install
   type "pkg"
   package_id "com.apple.pkg.update.os.10.8.2.12C60.combo"
+end
+
+# Install 10.8.2 variant update for new 2012 hardware
+dmg_package "OSXUpd10.8.2.dmg" do
+  app "OSXUpd10.8.2"
+  volumes_dir "OS X 10.8.2 Update"
+  dmg_name "OSXUpd10.8.2"
+  source "http://baratheon.class.wesleyan.edu/mac_os_x-10.8/os_x_updates-10.8/OSXUpd10.8.2.dmg"
+  checksum "89942e5886ea55e3b62d010b80ee996c5425c3411c3cb20493078aebe62f896c"
+  action :install
+  type "pkg"
+  package_id ""
 end
--- cookbooks/vlc/recipes/mac.rb
+++ cookbooks/vlc/recipes/mac.rb
@@ -6,13 +6,20 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+# Install package
 dmg_package "vlc-2.0.4" do
   app "VLC"
-  volumes_dir "vlc-2.0.3"
-  dmg_name "vlc-2.0.3"
+  volumes_dir "vlc-2.0.4"
+  dmg_name "vlc-2.0.4"
   dock_category "Multimedia"
   source "http://baratheon.class.wesleyan.edu/mac_os_x-10.8/vlc-2/vlc-2.0.4.dmg"
   checksum "d3770f25632aee8224ac17be383872ae05f684d69ee0f4c7e9d2b30778a8bfb5"
   action :install
   type "app"
+end
+
+# Add to dock
+dock_add "/Applications/VLC.app" do
+  all_users true
+  restart true  
 end
