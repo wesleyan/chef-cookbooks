0b9b54aae7b12505f5e8a3ee138f47320ee17b24
jclyons <jclyons@wesleyan.edu>
2012-11-01 13:53:49 -0400

    added homebrew

--- roles/mac.rb
+++ roles/mac.rb
@@ -1,9 +1,15 @@
 name "mac"
 description "mac image"
 
-run_list "recipe[prepare::mac]", "recipe[sudo]", "recipe[dmg]", 
+run_list "recipe[prepare::mac]", "recipe[sudo]", 
+          # XCode
+          "recipe[xcode::mac]", 
+          # Package providers
+          "recipe[dmg]", "recipe[homebrew]", 
+          # Desktop and Dock
+          "recipe[dock::mac]", "recipe[desktop_picture::mac]",
           # Core system updates
-          "recipe[osxupdate::mac]", "recipe[java::mac]", "recipe[printers::mac]"
+          "recipe[osxupdate::mac]", "recipe[java::mac]", "recipe[printers::mac]",
           # Browsers and runtimes
           "recipe[chrome::mac]", "recipe[firefox::mac]", "recipe[flash::mac]", "recipe[silverlight::mac]",
           # Core applications
@@ -15,16 +21,20 @@ run_list "recipe[prepare::mac]", "recipe[sudo]", "recipe[dmg]",
           # Audio applications
           "recipe[soundhack::mac]", "recipe[supercollider::mac]",
           # Adobe CS6 Suite
-          "recipe[photoshop::mac]", "recipe[illustrator::mac]", "recipe[indesign::mac]", "recipe[dreamweaver::mac]", "recipe[flashpro::mac]", "recipe[premierepro::mac]", "recipe[aftereffects::mac]", "recipe[acrobat::mac]",
+          "recipe[photoshop::mac]", "recipe[illustrator::mac]", "recipe[indesign::mac]", "recipe[dreamweaver::mac]", 
+          "recipe[flashpro::mac]", "recipe[premierepro::mac]", "recipe[aftereffects::mac]", "recipe[acrobat::mac]",
           # Development
-          "recipe[xcode::mac]", "recipe[eclipse::mac]", "recipe[aquamacs::mac]", "recipe[macvim::mac]"
+          "recipe[eclipse::mac]", "recipe[aquamacs::mac]", "recipe[macvim::mac]"
 
-#"recipe[remotedesktop::mac]",
+#"recipe[remotedesktop::mac]"--,
 
 default_attributes(
 "authorization" => {
   "sudo" => {
     "users" => ["administrator"],
     "passwordless" => true
+  },
+  "homebrew" => {
+    "user" => "administrator"
   }
 })
