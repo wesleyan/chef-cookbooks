3b85c42899d93738f3985d66fb6e98d2bae4271c
Robert Christensen <rchristensen@wesleyan.edu>
2012-11-01 18:13:28 -0400

    Added dock icon create to photoshop and premierepro

--- cookbooks/photoshop/CHANGELOG.md
+++ cookbooks/photoshop/CHANGELOG.md
@@ -2,16 +2,12 @@
 
 This file is used to list changes made in each version of photoshop.
 
-## 0.1.0:
-
-* Initial release of photoshop cookbook
-
 ## 0.2.0
 
 * Slightly rework cookbook because CS6 packages needed to be rebuilt
 * As a bonus, Photoshop CS6 is now version 13.0.1
+* Added dock icon creation
 
-- - - 
-Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+## 0.1.0:
 
-The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
+* Initial release of photoshop cookbook
--- cookbooks/premierepro/recipes/mac.rb
+++ cookbooks/premierepro/recipes/mac.rb
@@ -6,6 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+# Install package
 dmg_package "adobe_premiere_pro_cs6-6.0.2_Install.dmg" do
   app "adobe_premiere_pro_cs6-6.0.2_Install"
   volumes_dir "adobe_premiere_pro_cs6-6.0.2_Install"
@@ -15,4 +16,20 @@ dmg_package "adobe_premiere_pro_cs6-6.0.2_Install.dmg" do
   action :install
   type "pkg"
   package_id "com.adobe.Enterprise.install.A39ED404-FAD0-4310-8DC6-C00539826372"
+end
+
+# Create dock folder, if it does not exist
+dock_add "Adobe Creative Suite 6" do
+  all_users true
+  action :folder_create
+  show_as "list"
+  display_as "folder"
+  arrangement "name"
+end
+
+# Add icon to dock
+dock_add "/Applications/Adobe Premiere Pro CS6/Adobe Premiere Pro CS6.app" do
+  all_users true
+  group "Adobe Creative Suite 6"
+  restart true
 end
