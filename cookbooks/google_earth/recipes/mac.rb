c34d2a4e20c4be2314b3e37570f03f8f74ef09a7
jclyons <jclyons@wesleyan.edu>
2012-11-05 15:28:04 -0500

    Added google earth cookbook

---
+++ cookbooks/google_earth/recipes/mac.rb
@@ -0,0 +1,22 @@
+dmg_package "GoogleEarthMac-Intel-7.0.1.dmg" do
+  app "Google Earth"
+  volumes_dir "Google Earth"
+  dmg_name "GoogleEarthMac-Intel-7.0.1.dmg"
+  source "http://baratheon.class.wesleyan.edu/mac_os_x-10.8/google_earth-7/GoogleEarthMac-Intel-7.0.1.dmg"
+  checksum "6220fb17559f8ba8666908e5124812a16ac249f0163efd3c724e59bdf2788b31"
+  action :install
+  unmount false
+  type "app"
+end
+
+dmg_package "GoogleEarthMac-Intel-7.0.1.dmg" do
+  app "Google Earth Web Plug-in"
+  volumes_dir "Google Earth"
+  dmg_name "GoogleEarthMac-Intel-7.0.1.dmg"
+  source "http://baratheon.class.wesleyan.edu/mac_os_x-10.8/google_earth-7/GoogleEarthMac-Intel-7.0.1.dmg"
+  checksum "6220fb17559f8ba8666908e5124812a16ac249f0163efd3c724e59bdf2788b31"
+  action :install
+  destination "/Library/Internet Plug-ins/"
+  type "dir"
+  already_mounted true
+end
