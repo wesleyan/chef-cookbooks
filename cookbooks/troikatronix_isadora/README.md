d7e852614c9eb196345c94a82b6b11edb1ed0e33
Robert Christensen <rchristensen@wesleyan.edu>
2012-11-05 17:05:30 -0500

    Added isadora cookbook

---
+++ cookbooks/troikatronix_isadora/README.md
@@ -0,0 +1,14 @@
+Description
+===========
+Cookbook installs Isadora and it's required license file
+
+Requirements
+============
+OS X 10.4+
+
+Attributes
+==========
+
+Usage
+=====
+Use the cookbook!
---
+++ cookbooks/troikatronix_isadora/recipes/mac.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: troikatronix_isadora
+# Recipe:: mac
+#
+# Copyright 2012, Wesleyan University
+#
+# All rights reserved - Do Not Redistribute
+#
+# Install Isadora
+dmg_package "isadoracore13fc24-std.dmg" do
+  app "Isadora Core Installer"
+  volumes_dir "Isadora Core Installer"
+  dmg_name "isadoracore13fc24-std"
+  source "http://baratheon.class.wesleyan.edu/mac_os_x-10.8/troikatronix_isadora-1/sadoracore13fc24-std.dmg"
+  checksum "56af006265c382e72ab73d33cce141a126c79736467b0a231871cdac75661322"
+  action :install
+  type "pkg"
+end
