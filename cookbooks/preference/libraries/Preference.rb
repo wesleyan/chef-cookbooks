3bf982d63d2624e8ecabbbfd6db7422b9a507598
jclyons <jclyons@wesleyan.edu>
2012-11-07 17:10:07 -0500

    Moved PreferenceUtilities methods to common

--- cookbooks/preference/libraries/Preference.rb
+++ cookbooks/preference/libraries/Preference.rb
@@ -1,10 +1,3 @@
 module Preference
-  class MacUser 
-    attr_accessor :uid
-    attr_accessor :username
-    attr_accessor :user_dir
-    @uid = 0
-    @username = ""
-    @user_dir = ""
-  end
+
 end
