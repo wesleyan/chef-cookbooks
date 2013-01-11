78bb7251ebcf0c06feb82028dc805e6fba91a7b9
Robert Christensen <rchristensen@wesleyan.edu>
2013-01-11 18:27:21 -0500

    Updated Earth to 7.0.2, removed unneccessary plist

--- cookbooks/google_earth/CHANGELOG.md
+++ cookbooks/google_earth/CHANGELOG.md
@@ -2,6 +2,11 @@
 
 This file is used to list changes made in each version of google_earth.
 
+## 0.2.0:
+
+* Removed currently unneccesary preference copy.
+* Updated app to version 7.0.2.8415
+
 ## 0.1.0:
 
 * Initial release of google_earth
--- cookbooks/google_earth/recipes/mac.rb
+++ cookbooks/google_earth/recipes/mac.rb
@@ -7,34 +7,27 @@
 # All rights reserved - Do Not Redistribute
 #
 # Install package
-dmg_package "GoogleEarthMac-Intel-7.0.1.dmg" do
+dmg_package "GoogleEarthMac-Intel-7.0.2.8415.dmg" do
   app "Google Earth"
   volumes_dir "Google Earth"
-  dmg_name "GoogleEarthMac-Intel-7.0.1.dmg"
-  source "http://baratheon.class.wesleyan.edu/mac_os_x-10.8/google_earth-7/GoogleEarthMac-Intel-7.0.1.dmg"
-  checksum "6220fb17559f8ba8666908e5124812a16ac249f0163efd3c724e59bdf2788b31"
+  dmg_name "GoogleEarthMac-Intel-7.0.2.8415"
+  source "http://baratheon.class.wesleyan.edu/os_x-10.8/google_earth/GoogleEarthMac-Intel-7.0.2.8415.dmg"
+  checksum "228fb458ac569bc51884528d2cbdabde5df9f7c57fbe2d1d6661529a6132b67f"
   action :install
   unmount false
   type "app"
 end
 
 # Install web plugin
-dmg_package "GoogleEarthMac-Intel-7.0.1.dmg" do
-  app "Google Earth Web Plug-in"
+dmg_package "GoogleEarthMac-Intel-7.0.2.8415.dmg" do
+  app "Google Earth Web Plug-in.plugin"
   volumes_dir "Google Earth"
-  dmg_name "GoogleEarthMac-Intel-7.0.1.dmg"
-  source "http://baratheon.class.wesleyan.edu/mac_os_x-10.8/google_earth-7/GoogleEarthMac-Intel-7.0.1.dmg"
-  checksum "6220fb17559f8ba8666908e5124812a16ac249f0163efd3c724e59bdf2788b31"
+  dmg_name "GoogleEarthMac-Intel-7.0.2.8415"
+  source "http://baratheon.class.wesleyan.edu/os_x-10.8/google_earth/GoogleEarthMac-Intel-7.0.2.8415.dmg"
+  checksum "228fb458ac569bc51884528d2cbdabde5df9f7c57fbe2d1d6661529a6132b67f"
   action :install
   destination "/Library/Internet Plug-Ins/"
   type "dir"
   unmount true
-#  already_mounted true
-end
-
-cookbook_file "/tmp/com.google.GoogleEarthPlus.plist" 
-
-preference_copy "/tmp/com.google.GoogleEarthPlus.plist" do
-  convert_to_binary true
-  all_users true
+# already_mounted true
 end
