ac4fb110640e0857968d127bf64c76c0d0a389c0
Robert Christensen <rchristensen@wesleyan.edu>
2013-01-11 18:30:07 -0500

    Fixed various pkg URLs

--- cookbooks/ibm_spss/recipes/mac.rb
+++ cookbooks/ibm_spss/recipes/mac.rb
@@ -14,7 +14,7 @@ dmg_package "SPSS_Statistics_21_mac_silent.dmg" do
   app "SPSS_Statistics_Installer"
   volumes_dir "SPSSStatistics"
   dmg_name "SPSS_Statistics_21_mac_silent"
-  source "http://baratheon.class.wesleyan.edu/mac_os_x-10.8/ibm_spss-21/SPSS_Statistics_21_mac_silent.dmg"
+  source "http://baratheon.class.wesleyan.edu/os_x-10.8/ibm_spss/SPSS_Statistics_21_mac_silent.dmg"
   checksum "d5c74264d02dcca7862eb677b05e10d9c2ce82c14979ca0b0a26784f2fbaf17b"
   action :install
   type "custom"
--- cookbooks/sassafras_k2/recipes/mac.rb
+++ cookbooks/sassafras_k2/recipes/mac.rb
@@ -11,8 +11,8 @@ dmg_package "K2Client-7.0.1.3.dmg" do
   app "K2Client"
   volumes_dir "K2Client"
   dmg_name "K2Client-7.0.1.3"
-  source "http://baratheon.class.wesleyan.edu/mac_os_x-10.8/sassafras_k2-7/K2Client-7.0.1.3.dmg"
-  checksum "dec2453dd47214952eb457b392acfb1aa817f0c5483f6a19a0e9ce4a048109cf"
+  source "http://baratheon.class.wesleyan.edu/os_x-10.8/sassafras_k2/K2Client-7.0.1.3.dmg"
+  checksum "24caabdf98a951ed2af56f511e89d457f01cb9ad86242afa17410b7e34b9e262"
   action :install
   type "mpkg"
   package_id "com.sassafras.pkg.KeyAccess"
