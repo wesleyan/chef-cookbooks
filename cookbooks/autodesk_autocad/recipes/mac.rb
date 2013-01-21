254d163225361c2136beb574603123c5eaa6c9c0
Robert Christensen <rchristensen@wesleyan.edu>
2013-01-21 11:12:41 -0500

    Updated AutoCAD pkg

--- cookbooks/autodesk_autocad/recipes/mac.rb
+++ cookbooks/autodesk_autocad/recipes/mac.rb
@@ -7,16 +7,17 @@
 # All rights reserved - Do Not Redistribute
 #
 # Install package
-dmg_package "autocad_2012_sp3_english_mac_osx.dmg" do
-  app "Install AutoCAD 2012 for Mac"
-  volumes_dir "AutoCAD 2012 for Mac Installation"
-  dmg_name "autocad_2012_sp3_english_mac_osx"
-  source "http://baratheon.class.wesleyan.edu/os_x-10.8/autodesk_autocad/autocad_2012_sp3_english_mac_osx.dmg"
-  checksum "35be3b0e656ebb0bbf038cbcab3aeb0191992def87f650d317ec2c6b0cdf9177"
+dmg_package "autocad-2012SP3.dmg" do
+  app "autocad-2012SP3"
+  volumes_dir "autocad-2012SP3"
+  dmg_name "autocad-2012SP3"
+  source "http://baratheon.class.wesleyan.edu/os_x-10.8/autodesk_autocad/autocad-2012SP3.dmg"
+  checksum "dca9c9d54da90348d21f7ba754efcc0fca6b9411d014b4cb1473669550ab3a71"
   action :install
   type "pkg"
-  package_id "com.autodesk.autocad2012"
+  package_id ""
 end
 
+
 # Insert license file
-cookbook_file "/Applications/Autodesk/AutoCAD 2012/AutoCAD.app/Contents/licpath.lic"
+#cookbook_file "/Applications/Autodesk/AutoCAD 2012/AutoCAD.app/Contents/licpath.lic"
