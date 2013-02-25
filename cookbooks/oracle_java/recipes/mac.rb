2a7b0ab5a1a087fb57e3866f58cb8121554a4d1f
Robert Christensen <rchristensen@wesleyan.edu>
2013-02-25 14:13:01 -0500

    Updated Java and iTunes
    Oracle and Apple Java updated to latest patchlevels, iTunes to 11.0.2

--- cookbooks/oracle_java/recipes/mac.rb
+++ cookbooks/oracle_java/recipes/mac.rb
@@ -2,29 +2,29 @@
 # Cookbook Name:: java
 # Recipe:: mac
 #
-# Copyright 2012, Wesleyan University
+# Copyright 2013, Wesleyan University
 #
 # All rights reserved - Do Not Redistribute
 #
-# Installs system-level Java 6 Runtime for applications
-dmg_package "JavaForOSX2012-006" do
+# Installs system-level Java 6 Runtime for applications, disables Java 6 web plug-in
+dmg_package "JavaForOSX2013-001" do
   app "JavaForOSX"
-  volumes_dir "Java for OS X 2012-006"
-  dmg_name "JavaForOSX2012-006"
-  source "http://baratheon.class.wesleyan.edu/os_x-10.8/apple_java/JavaForOSX2012-006.dmg"
-  checksum "6d947f4a20e8f7ee35bb460675ad0b930f8acc15ee0b0dbfc791a7a02abed6c0"
+  volumes_dir "Java for OS X 2013-001"
+  dmg_name "JavaForOSX2013-001"
+  source "http://baratheon.class.wesleyan.edu/os_x-10.8/apple_java/JavaForOSX2013-001.dmg"
+  checksum "5c6f709b747a9c067580accf260a18cadba13ad16203adf8238929b5b2869f8b"
   action :install
   type "pkg"
   package_id "com.apple.pkg.JavaForMacOSX107"
 end
 
-# Installs Java 7 web plugin for Browsers
-dmg_package "jre-7u11-macosx-x64.dmg" do
-  app "Java 7 Update 11"
-  volumes_dir "Java 7 Update 11"
-  dmg_name "jre-7u11-macosx-x64"
-  source "http://baratheon.class.wesleyan.edu/os_x-10.8/oracle_java/jre-7u11-macosx-x64.dmg"
-  checksum "089410645d13b2dd9e6e1551cc7ea0e74d4a26c4f5d50eb7f8a780ff25ac8b71"
+# Installs Java 7 web plug-in for browsers
+dmg_package "jre-7u15-macosx-x64.dmg" do
+  app "Java 7 Update 15"
+  volumes_dir "Java 7 Update 15"
+  dmg_name "jre-7u15-macosx-x64"
+  source "http://baratheon.class.wesleyan.edu/os_x-10.8/oracle_java/jre-7u15-macosx-x64.dmg"
+  checksum "62be342a4ed754f14fad58dbe2ed22820971f271840beb685e232631293c8e96"
   action :install
   type "pkg"
   package_id "com.oracle.jre"
