79c10a261f488ded657b0399e847b8a3e23a3f2a
Robert Christensen <rchristensen@wesleyan.edu>
2013-02-25 17:19:29 -0500

    Updated Adobe Acrobat Pro to 11.0.2

--- cookbooks/adobe_acrobat_pro/recipes/mac.rb
+++ cookbooks/adobe_acrobat_pro/recipes/mac.rb
@@ -6,6 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+# Install base package
 dmg_package "adobe_acrobat_pro_xi-11.0.dmg" do
   app "adobe_acrobat_pro_xi-11.0"
   volumes_dir "adobe_acrobat_pro_xi-11.0"
@@ -16,6 +17,31 @@ dmg_package "adobe_acrobat_pro_xi-11.0.dmg" do
   type "pkg"
   package_id "com.adobe.acrobat.11.viewer.app.pkg.MUI"
 end
+
+# Install first point release update, which is a dependency of all following point releases
+dmg_package "AcrobatUpd11001.dmg" do
+  app "AcrobatUpd11001"
+  volumes_dir "AcrobatUpd11001"
+  dmg_name "AcrobatUpd11001"
+  source "http://baratheon.class.wesleyan.edu/os_x-10.8/adobe_acrobat_pro_xi/AcrobatUpd11001.dmg"
+  checksum "dab9ab5f9705ffde175b07205174736cb14c01755ef83ab376691fa34081feda"
+  action :install
+  type "pkg"
+  package_id "com.adobe.acrobat.a11.AcrobatUpd11001"
+end
+
+# Install the latest security patch
+dmg_package "AcrobatSecUpd11002.dmg" do
+  app "AcrobatSecUpd11002"
+  volumes_dir "AcrobatSecUpd11002"
+  dmg_name "AcrobatSecUpd11002"
+  source "http://baratheon.class.wesleyan.edu/os_x-10.8/adobe_acrobat_pro_xi/AcrobatSecUpd11002.dmg"
+  checksum "88edf1efee233fbd1319b1ded95e766e7a942a2e843d293f248482e40f082077"
+  action :install
+  type "pkg"
+  package_id "com.adobe.acrobat.a11.AcrobatSecUpd11002"
+end
+
 # Download icon
 cookbook_file "/tmp/adobe.png"
 
