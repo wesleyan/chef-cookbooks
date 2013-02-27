650181bb2bb604a50068ad36ac5a5c14aa56e6f2
mdietz94 <mdietz@wesleyan.edu>
2013-02-27 11:34:14 -0500

    Audacity updated to version 2.0.3

--- cookbooks/audacity/metadata.rb
+++ cookbooks/audacity/metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "rchristensen"
 license          "All rights reserved"
 description      "Installs Audacity and LAME/FFmpeg plugins"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.3.0"
