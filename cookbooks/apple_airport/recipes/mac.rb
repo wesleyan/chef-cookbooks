8eb21c4a034e918dbaf3e7cd5e6abea6405db269
Robert Christensen <rchristensen@wesleyan.edu>
2013-02-27 12:51:07 -0500

    Added AirPort Utility

---
+++ cookbooks/apple_airport/recipes/mac.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: apple_airport
+# Recipe:: mac
+#
+# Copyright 2013, Wesleyan University
+#
+# All rights reserved - Do Not Redistribute
+#
+dmg_package "Apple AirPort Utility" do
+  app "AirPortUtility"
+  volumes_dir "AirPortUtility"
+  dmg_name "AirPortUtility6.2"
+  source "http://baratheon.class.wesleyan.edu/os_x-10.8/apple_airport/AirPortUtility6.2.dmg"
+  checksum "eaaf25a022719e62ade84148ed8553bfe49a700d0662ab9a17f95e3f5bb2ae51"
+  action :install
+  type "pkg"
+  package_id "com.apple.pkg.AirPortUtility"
+end
