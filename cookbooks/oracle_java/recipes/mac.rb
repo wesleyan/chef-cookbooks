7b2d84667ca87c5378516d79e545d97ddb860767
mdietz94 <mdietz@wesleyan.edu>
2013-03-05 13:34:25 -0500

    Java version is now stored in its plist, dmg does plist parsing with checks for success, and mac role now installs xquartz before python as is necessary for the tk installation

--- cookbooks/oracle_java/recipes/mac.rb
+++ cookbooks/oracle_java/recipes/mac.rb
@@ -29,3 +29,28 @@ dmg_package "jre-7u15-macosx-x64.dmg" do
   type "pkg"
   package_id "com.oracle.jre"
 end
+
+chef_gem "plist"
+ruby_block "updateJavaVersion" do
+  block do
+    require 'plist'
+    # calls java -version and parses it for the version information, for some reason java writes to stderr
+    # so we redirect STDERR to STDOUT
+    # also, Gem::Version does not understand '_', so we replace it with '.'
+    v = `java -version 2>&1`.split("\n")[0].split[-1][1...-1].gsub('_','.')
+    # converts the plist to xml format and parses it
+    currPKG = Plist::parse_xml(`plutil -convert xml1 -o - /var/db/receipts/com.oracle.jre.plist`)
+    # in some instances parse_xml does not work properly and returns a string, if that is what happens, just parse it again
+    currPKG = Plist::parse_xml(currPKG) if (currPKG.class == String)
+    # set the PackageVersion
+    currPKG['PackageVersion'] = v
+    # write to a temporary file
+    f = ::File.open("/tmp/com.oracle.jre.plist","w")
+    f.puts Plist::Emit.dump(currPKG)
+    f.close
+    # convert it back to binary and replace the original
+    system("rm -rf /var/db/receipts/com.oracle.jre.plist")
+    system("plutil -convert binary1 -o /var/db/receipts/com.oracle.jre /tmp/com.oracle.jre.plist")
+    system("rm -rf /tmp/com.oracle.jre.plist")
+  end
+end
