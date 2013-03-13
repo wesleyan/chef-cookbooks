d2df86f845c9ac7914902512a8454aa585d5e850
Robert Christensen <rchristensen@wesleyan.edu>
2013-03-13 14:59:22 -0400

    Added Universal Type Client

---
+++ cookbooks/extensis_utc/CHANGELOG.md
@@ -0,0 +1,8 @@
+# CHANGELOG for extensis_utc
+
+This file is used to list changes made in each version of extensis_utc.
+
+## 0.1.0:
+
+* Initial release of extensis_utc cookbook
+* Current app version: 3.3.0
---
+++ cookbooks/extensis_utc/recipes/mac.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: extensis_utc
+# Recipe:: mac
+#
+# Copyright 2013, Wesleyan University
+#
+# All rights reserved - Do Not Redistribute
+#
+# Install client package
+dmg_package "Universal Type Client" do
+  app "Universal Type Client"
+  volumes_dir "Universal Type Client"
+  dmg_name "universal_type_client-3.3.0"
+  source "http://baratheon.class.wesleyan.edu/os_x-10.8/extensis_utc/universal_type_client-3.3.0.dmg"
+  checksum "7e325f7186de90dde22958a16aceca13f296be53f5aac32a88adf213c57c3fd5"
+  action :install
+  type "pkg"
+  package_id "com.extensis.UniversalTypeClient.universalTypeClient33.UniversalTypeClient.pkg<"
+end
