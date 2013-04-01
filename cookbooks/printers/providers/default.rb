5b8e6784279109c72629ce704a6ac11607824c76
Max Dietz <mdietz@Maxs-iMac.local>
2013-04-01 13:55:48 -0400

    dmg reverted, printer attributes, etc fixed

--- cookbooks/printers/providers/default.rb
+++ cookbooks/printers/providers/default.rb
@@ -5,7 +5,7 @@
 
 action :add do
   if new_resource.ip
-    system("lpadmin -p #{new_resource.name} -v ipp://#{new_resource.ip}/ -m \"#{new_resource.model}\" -D #{new_resource.name} -E -o HPOption_Duplexer=True -o Duplex=DuplexNoTumble -o Duplexer=Installed")
+    system("lpadmin -p #{new_resource.name} -v lpd://#{new_resource.ip} -m \"#{new_resource.model}\" -D #{new_resource.name} -E")
   else
 	  system("lpadmin -p #{new_resource.name} -v lpd://falcon.wesleyan.edu/#{new_resource.name} -m \"#{new_resource.model}\" -D #{new_resource.name} -E -o HPOption_Duplexer=True -o Duplex=DuplexNoTumble -o Duplexer=Installed")
   end
