3233b0165b0e0a9392264a23ce333c48748f368e
Robert Christensen <rchristensen@wesleyan.edu>
2013-04-02 14:44:41 -0400

    Fixed Java version

--- cookbooks/oracle_java/recipes/mac.rb
+++ cookbooks/oracle_java/recipes/mac.rb
@@ -16,7 +16,7 @@ dmg_package "Apple Java" do
   action :install
   type "pkg"
   package_id "com.apple.pkg.JavaForMacOSX107"
-  version "2013.002"
+  version "2013.2.0"
 end
 
 # Installs Java 7 web plug-in for browsers
@@ -29,32 +29,5 @@ dmg_package "Oracle Java" do
   action :install
   type "pkg"
   package_id "com.oracle.jre"
-  version "7.0u17"
+  version "7.0.17"
 end
-
-# Is this needed anymore? Thought it was implemented in the DMG provider?
-#
-#chef_gem "plist"
-#ruby_block "updateJavaVersion" do
-#  block do
-#    require 'plist'
-#    # calls java -version and parses it for the version information, for some reason java writes to stderr
-#    # so we redirect STDERR to STDOUT
-#    # also, Gem::Version does not understand '_', so we replace it with '.'
-#    v = `java -version 2>&1`.split("\n")[0].split[-1][1...-1].gsub('_','.')
-#    # converts the plist to xml format and parses it
-#    currPKG = Plist::parse_xml(`plutil -convert xml1 -o - /var/db/receipts/com.oracle.jre.plist`)
-#    # in some instances parse_xml does not work properly and returns a string, if that is what happens, just parse it again
-#    currPKG = Plist::parse_xml(currPKG) if (currPKG.class == String)
-#    # set the PackageVersion
-#    currPKG['PackageVersion'] = v
-#    # write to a temporary file
-#    f = ::File.open("/tmp/com.oracle.jre.plist","w")
-#    f.puts Plist::Emit.dump(currPKG)
-#    f.close
-#    # convert it back to binary and replace the original
-#    system("rm -rf /var/db/receipts/com.oracle.jre.plist")
-#    system("plutil -convert binary1 -o /var/db/receipts/com.oracle.jre /tmp/com.oracle.jre.plist")
-#    system("rm -rf /tmp/com.oracle.jre.plist")
-#  end
-#end
