7b196784a36d09ad3e313f4e51b56e680744c7fc
Max Dietz <mdietz@Maxs-iMac.local>
2013-04-08 11:54:11 -0400

    Apple Xcode updated to suppress all first-launch messages
    DevSDK updated, plist modified to suppress reinstallation

--- cookbooks/apple_xcode/recipes/mac.rb
+++ cookbooks/apple_xcode/recipes/mac.rb
@@ -37,20 +37,11 @@ dmg_package "Xcode DevSDK" do
   volumes_dir "Xcode DevSDK"
   dmg_name "xcode-devsdk"
   source "http://baratheon.class.wesleyan.edu/os_x-10.8/apple_xcode/xcode-devsdk.dmg"
-  checksum "9381e9621bc922a6bd35b52342e9e2bff7ddb66ae5ca3b890cbf1cda6da49d44"
+  checksum "19061cff7c937297c309cd8c28a8821c1ba18b7b16caf291a643d801dc5347a0"
   action :install
   type "pkg"
   package_id "com.apple.pkg.DevSDK"
   version "4.6.1.0413"
 end
 
-# Place plist file in default user profile, required for automated Xcode license acceptance
-cookbook_file "0313 XCode License" do
-  backup false
-  path "/System/Library/User Template/English.lproj/Library/Preferences/com.apple.dt.Xcode.plist"
-  source "com.apple.dt.Xcode.plist"
-  mode 0600
-  :create
-end
-
-# execute "xcode-select -switch /usr/bin"
+cookbook_file "/System/Library/User Template/English.lproj/Library/Preferences/com.apple.dt.Xcode.plist"
