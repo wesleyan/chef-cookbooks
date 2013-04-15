443259a12d0496179178aeb10b1199a56c761390
Max Dietz <mdietz@Maxs-iMac.local>
2013-04-15 14:04:54 -0400

    fixed settings

--- cookbooks/eset_nod32/recipes/mac.rb
+++ cookbooks/eset_nod32/recipes/mac.rb
@@ -26,12 +26,12 @@ eset_nod32_config "ESET Settings" do
 	av_update_failsafe_password "u2er7jnsaa"
 	av_update_server_list "http://gloss.wesleyan.edu:2221"
 	racl_server_addr "gloss.wesleyan.edu"
-	racl_server_port "2222"
-	racl_interval "10"
+	racl_server_port 2222
+	racl_interval 10
 	privileged_users "administrator:labuser:root"
-	samples_enabled "yes"
+	samples_enabled true
 	samples_send_files "update"
 	samples_send_stats "update"
 	samples_send_target "ras:eset"
-	av_scan_app_unwanted "yes"
+	av_scan_app_unwanted true
 end
