acd0f502deff6da5d8c719a2572eaaf905379ae2
Max Dietz <mdietz@Maxs-iMac.local>
2013-04-15 13:45:10 -0400

    eset nod32 provider created

---
+++ cookbooks/eset_nod32/providers/config.rb
@@ -0,0 +1,23 @@
+action :config do
+  system("/Applications/.esets/Contents/MacOS/esets_set  --section=global 'av_update_server=\"#{new_resource.av_update_server}\"'")
+  system("/Applications/.esets/Contents/MacOS/esets_set  --section=global 'av_update_failsafe_username=\"#{new_resource.av_update_failsafe_username}\"'")
+  system("/Applications/.esets/Contents/MacOS/esets_set  --section=global 'av_update_failsafe_password=\"#{new_resource.av_update_failsafe_password}\"'")
+  system("/Applications/.esets/Contents/MacOS/esets_set  --section=global 'av_update_server_list=\"#{new_resource.av_update_server_list}\"'")
+  system("/Applications/.esets/Contents/MacOS/esets_set  --section=global 'racl_server_addr=\"#{new_resource.racl_server_addr}\"'")
+  system("/Applications/.esets/Contents/MacOS/esets_set  --section=global 'racl_server_port=#{new_resource.racl_server_port}'")
+  system("/Applications/.esets/Contents/MacOS/esets_set  --section=global 'racl_interval=#{new_resource.racl_interval}'")
+  system("/Applications/.esets/Contents/MacOS/esets_set  --section=global 'privileged_users=\"#{new_resource.privileged_users}\"'")
+  if new_resource.samples_enabled
+    system("/Applications/.esets/Contents/MacOS/esets_set  --section=global 'samples_enabled=yes'")
+  else
+    system("/Applications/.esets/Contents/MacOS/esets_set  --section=global 'samples_enabled=no'")
+  end
+  system("/Applications/.esets/Contents/MacOS/esets_set  --section=global 'samples_send_files=\"#{new_resource.samples_send_files}\"'")
+  system("/Applications/.esets/Contents/MacOS/esets_set  --section=global 'samples_send_stats=\"#{new_resource.samples_send_stats}\"'")
+  system("/Applications/.esets/Contents/MacOS/esets_set  --section=global 'samples_send_target=\"#{new_resource.samples_send_target}\"'")
+  if new_resource.av_scan_app_unwanted
+    system("/Applications/.esets/Contents/MacOS/esets_set  --section=global 'av_scan_app_unwanted=yes'")
+  else
+    system("/Applications/.esets/Contents/MacOS/esets_set  --section=global 'av_scan_app_unwanted=no'")
+  end
+end
--- cookbooks/eset_nod32/recipes/mac.rb
+++ cookbooks/eset_nod32/recipes/mac.rb
@@ -20,7 +20,7 @@ dmg_package "ESET NOD32" do
 end
 
 # Configure ESET with our fancy method
-configure_eset "ESET Settings" do
+eset_nod32_config "ESET Settings" do
 	av_update_server "http://gloss.wesleyan.edu:2221"
 	av_update_failsafe_username "EAV-33650109"
 	av_update_failsafe_password "u2er7jnsaa"
@@ -35,20 +35,3 @@ configure_eset "ESET Settings" do
 	samples_send_target "ras:eset"
 	av_scan_app_unwanted "yes"
 end
-
-# Configure ESET settings via command line application "esets_set"
-#	/Applications/.esets/Contents/MacOS/esets_set  --section=global 'av_update_server="http://gloss.wesleyan.edu:2221"'
-#	/Applications/.esets/Contents/MacOS/esets_set  --section=global 'av_update_failsafe_username="EAV-33650109"'
-#	/Applications/.esets/Contents/MacOS/esets_set  --section=global 'av_update_failsafe_password="u2er7jnsaa"'
-#	/Applications/.esets/Contents/MacOS/esets_set  --section=global 'av_update_server_list="http://gloss.wesleyan.edu:2221"'
-#	/Applications/.esets/Contents/MacOS/esets_set  --section=global 'racl_server_addr="gloss.wesleyan.edu"'
-#	/Applications/.esets/Contents/MacOS/esets_set  --section=global 'racl_server_port=2222'
-#	/Applications/.esets/Contents/MacOS/esets_set  --section=global 'racl_interval=10'
-#	/Applications/.esets/Contents/MacOS/esets_set  --section=global 'privileged_users="administrator:labuser:root"'
-#	/Applications/.esets/Contents/MacOS/esets_set  --section=global 'samples_enabled=yes'
-#	/Applications/.esets/Contents/MacOS/esets_set  --section=global 'samples_send_files="update"'
-#	/Applications/.esets/Contents/MacOS/esets_set  --section=global 'samples_send_stats="update"'
-#	/Applications/.esets/Contents/MacOS/esets_set  --section=global 'samples_send_target="ras:eset"'
-#	/Applications/.esets/Contents/MacOS/esets_set  --section=global 'av_scan_app_unwanted=yes'
-#
-#exit 0
