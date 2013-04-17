c220436001a58845f1fbd3ee25666ff5511a8445
Max Dietz <mdietz@Maxs-iMac.local>
2013-04-17 11:49:01 -0400

    printer sharing disabled

--- cookbooks/printers/providers/default.rb
+++ cookbooks/printers/providers/default.rb
@@ -6,9 +6,9 @@
 action :add do
   unless `lpinfo -v`.index(new_resource.name)
     if new_resource.ip
-      system("lpadmin -p #{new_resource.name} -v lpd://#{new_resource.ip} -m \"#{new_resource.model}\" -D #{new_resource.name} -E")
+      system("lpadmin -p #{new_resource.name} -v lpd://#{new_resource.ip} -m \"#{new_resource.model}\" -D #{new_resource.name} -o printer-is-shared=false -E")
     else
-	    system("lpadmin -p #{new_resource.name} -v lpd://falcon.wesleyan.edu/#{new_resource.name} -m \"#{new_resource.model}\" -D #{new_resource.name} -E -o HPOption_Duplexer=True -o Duplex=DuplexNoTumble -o Duplexer=Installed")
+	    system("lpadmin -p #{new_resource.name} -v lpd://falcon.wesleyan.edu/#{new_resource.name} -m \"#{new_resource.model}\" -D #{new_resource.name} -E -o printer-is-shared=false -o HPOption_Duplexer=True -o Duplex=DuplexNoTumble -o Duplexer=Installed")
     end
   end
 end
