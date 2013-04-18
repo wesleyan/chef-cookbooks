b5ee868524b5fc6ef8695f2f9ae9bed44cbd1f62
Max Dietz <mdietz@Maxs-iMac.local>
2013-04-18 12:03:04 -0400

    updated metadata

--- cookbooks/printers/metadata.rb
+++ cookbooks/printers/metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "software@wesleyan.edu"
 license          "All rights reserved"
 description      "Installs/Configures Campus Printers"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.4.0"
+version          "0.5.0"
