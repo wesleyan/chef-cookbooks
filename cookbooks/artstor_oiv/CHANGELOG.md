301fe931bb7d93e0dd17809eb09c2d75fc6a18a1
Robert Christensen <rchristensen@wesleyan.edu>
2013-05-17 14:13:18 -0400

    Added ARTstor OIV
    Need work out a method to automate activation, but low priority

---
+++ cookbooks/artstor_oiv/CHANGELOG.md
@@ -0,0 +1,8 @@
+# CHANGELOG for artstor_oiv
+
+This file is used to list changes made in each version of artstor_oiv.
+
+## 0.1.0:
+
+* Initial release of artstor_oiv
+* Current app version: 3.1.3
---
+++ cookbooks/artstor_oiv/metadata.rb
@@ -0,0 +1,7 @@
+name             'artstor_oiv'
+maintainer       'Wesleyan University'
+maintainer_email 'software@wesleyan.edu'
+license          'All rights reserved'
+description      'Installs/Configures ARTstor OIV'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
