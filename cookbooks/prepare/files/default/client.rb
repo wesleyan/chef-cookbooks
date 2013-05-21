c06b71f7472f8a360aef192f541248c1a5e49799
Max Dietz <mdietz@wesleyan.edu>
2013-05-21 10:46:25 -0400

    fixed client.rb

--- cookbooks/prepare/files/default/client.rb
+++ cookbooks/prepare/files/default/client.rb
@@ -1,4 +1,4 @@
 log_level        :info
 log_location     STDOUT
-chef_server_url  'https://baratheon.class.wesleyan.edu:8443'
+chef_server_url  'https://ims-chef.wesleyan.edu:8443'
 validation_client_name 'chef-validator'
