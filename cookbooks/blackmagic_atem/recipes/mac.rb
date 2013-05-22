999457374e631adac817ee4b91d2bc9384a44e81
Robert Christensen <rchristensen@wesleyan.edu>
2013-05-22 12:21:20 -0400

    Added Blackmagic ATEM & Desktop Video
    Still need to fix pkg ids

---
+++ cookbooks/blackmagic_atem/recipes/mac.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: blackmagic_atem
+# Recipe:: mac
+#
+# Copyright 2013, Wesleyan University
+#
+# All rights reserved - Do Not Redistribute
+#
+# Install the package
+dmg_package "Blackmagic ATEM" do
+  app "Blackmagic ATEM Switchers Installer 4.0"
+  volumes_dir "Blackmagic ATEM Switchers"
+  dmg_name "Blackmagic_ATEM_Switchers_4.0"
+  source "http://ims-chef.wesleyan.edu/os_x/blackmagic_atem/Blackmagic_ATEM_Switchers_4.0.dmg"
+  checksum "e6e4dbb0d17282bf7831a4be5db450d5ef29fc082952744c5971cc2f6bf9ce50"
+  action :install
+  type "pkg"
+  #package_id ""
+  #version "4.3.0"
+end
---
+++ cookbooks/blackmagic_desktop_video/CHANGELOG.md
@@ -0,0 +1,8 @@
+# CHANGELOG for blackmagic_desktop_video
+
+This file is used to list changes made in each version of blackmagic_desktop_video.
+
+## 0.1.0:
+
+* Initial release of blackmagic_desktop_video
+* Current app version: 9.7.2
