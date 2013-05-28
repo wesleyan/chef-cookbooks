268e393b169f4791bd2985c2607715880e49dd59
Max Dietz <maxwell.r.dietz@gmail.com>
2013-05-28 13:05:37 -0300

    updated versions, removed hotfix
    Hotfix was introducing new build problems.

--- cookbooks/python/recipes/mac.rb
+++ cookbooks/python/recipes/mac.rb
@@ -10,19 +10,14 @@ directory "/usr/local/include" do
   recursive true
 end
 
-# Fixes an error in building Tkinter, when the issue linked below is resolved
-# this will no longer be necessary
-execute "Hotfix for Tkinter error (https://github.com/mxcl/homebrew/issues/19099)" do
-  command "ln -s /opt/X11/include/X11 /usr/local/include/X11"
-end
 
 package "python" do
-  version "2.7.3"
+  version "2.7.5"
   options "--with-brewed-tk"
 end
 
 
 package "python3" do
-  version "3.3.0"
+  version "3.3.2"
 end
 
