57cc744b9ec62d4ba3a26c059d2d0f35d16cef6c
Max Dietz <maxwell.r.dietz@gmail.com>
2013-05-28 13:06:16 -0300

    directory for hotfix removed
    should have been removed in previous commit

--- cookbooks/python/recipes/mac.rb
+++ cookbooks/python/recipes/mac.rb
@@ -6,18 +6,11 @@ package "tcl-tk" do
   options "--enable-threads --with-x11"
 end
 
-directory "/usr/local/include" do
-  recursive true
-end
-
-
 package "python" do
   version "2.7.5"
   options "--with-brewed-tk"
 end
 
-
 package "python3" do
   version "3.3.2"
 end
-
