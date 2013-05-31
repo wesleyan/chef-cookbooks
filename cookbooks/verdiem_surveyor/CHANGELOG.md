0ef19801a87ca01db8ce01c8c297e15f573fbd63
Max Dietz <maxwell.r.dietz@gmail.com>
2013-05-31 08:03:23 -0400

    Revert "added verdiem surveyor"
    This reverts commit bbf6791bf56dbb013755ad795e1210bdc54108f1.

--- cookbooks/verdiem_surveyor/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# CHANGELOG for ccp4
-
-This file is used to list changes made in each version of Surveyor.
-
-## 0.1.0:
-
-* Initial release of verdiem_surveyor cookbook
--- cookbooks/verdiem_surveyor/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "Wesleyan University"
-maintainer_email "mdietz@wesleyan.edu"
-license          "All rights reserved"
-description      "Installs Surveyor"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
