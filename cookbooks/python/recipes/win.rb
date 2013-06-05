e636320e00ed14d57b117a9f4f16b80cbb5df1d3
Max Dietz <mdietz@wesleyan.edu>
2013-06-05 16:22:01 -0400

    added python windows recipe

---
+++ cookbooks/python/recipes/win.rb
@@ -0,0 +1,11 @@
+# Python 2,3 windows installer
+
+windows_package "Python 2.7" do
+  source "http://ims-chef.wesleyan.edu/windows/python/python-2.7.5.msi"
+  version "2.7.5"
+end
+
+windows_package "Python 3.3" do
+  source "http://ims-chef.wesleyan.edu/windows/python/python-3.3.2.msi"
+  version "3.3.2"
+end
