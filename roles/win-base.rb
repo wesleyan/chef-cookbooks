737153f7401618096c901adc146e84b7b8d56297
Max Dietz <mdietz@wesleyan.edu>
2013-06-05 15:59:27 -0400

    added vlc

--- roles/win-base.rb
+++ roles/win-base.rb
@@ -1,3 +1,3 @@
 name "win-base"
 description "Base role for Windows deployment"
-run_list "recipe[windows]", "recipe[7-zip]", "recipe[office::pc]"
+run_list "recipe[windows]", "recipe[7-zip]", "recipe[videolan_vlc::win]"
