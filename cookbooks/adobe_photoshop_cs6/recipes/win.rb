b22774c5cc83ab1ce47e346068dbcdf395f4b4c3
Max Dietz <mdietz@wesleyan.edu>
2013-06-07 11:43:18 -0400

    created adobe photoshop recipe for windows

---
+++ cookbooks/adobe_photoshop_cs6/recipes/win.rb
@@ -0,0 +1,31 @@
+# Installs Adobe Photoshop CS6 for Windows
+
+remote_file "C:/adobe_photoshop_cs6.zip" do
+	backup false
+	source "http://ims-chef.wesleyan.edu/windows/adobe_photoshop_cs6/adobe_photoshop_cs6.zip"
+	not_if { ::File.exists? "#{Chef::Config[:file_cache_path]}/Receipts/Adobe Photoshop CS6" }
+end
+
+execute 'Unzip Installer' do
+	command 'C:\7-zip\7z.exe x -y adobe_photoshop_cs6.zip'
+	not_if { ::File.exists? "#{Chef::Config[:file_cache_path]}/Receipts/Adobe Photoshop CS6" }
+end
+
+execute 'Install Adobe Photoshop' do
+	command 'C:\adobe_photoshop_cs6\install.bat'
+	not_if { ::File.exists? "#{Chef::Config[:file_cache_path]}/Receipts/Adobe Photoshop CS6" }
+end
+
+execute 'Remove installer files' do
+	command 'rm -rf C:\adobe_photoshop_cs6 adobe_photoshop_cs6.zip'
+	not_if { ::File.exists? "#{Chef::Config[:file_cache_path]}/Receipts/Adobe Photoshop CS6" }
+end
+
+ruby_block 'Create Package Receipt' do
+	block do
+		::File.open("#{Chef::Config[:file_cache_path]}/Receipts/Adobe Photoshop CS6", "w") do |f|
+			f << "1.0.0"
+		end
+	end
+	not_if { ::File.exists? "#{Chef::Config[:file_cache_path]}/Receipts/Adobe Photoshop CS6" }
+end
