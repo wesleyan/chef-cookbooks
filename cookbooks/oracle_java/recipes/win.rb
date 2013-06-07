50741ed203c315a65d9cbbbca3653a3ac18916a1
Max Dietz <mdietz@wesleyan.edu>
2013-06-07 14:19:52 -0400

    added windows java jdk recipe

---
+++ cookbooks/oracle_java/recipes/win.rb
@@ -0,0 +1,6 @@
+# Installs Oracle Java JDK for Windows
+
+windows_package "Oracle Java JDK" do
+  source "http://ims-chef.wesleyan.edu/windows/oracle_java/jdk-7u21-windows-x64.exe"
+  version "7.21"
+end
--- roles/win-base.rb
+++ roles/win-base.rb
@@ -9,4 +9,5 @@ run_list "recipe[windows]",
   "recipe[adobe_after_effects_cs6::win]",
   "recipe[adobe_illustrator_cs6::win]",
   "recipe[adobe_premiere_pro_cs6::win]",
-  "recipe[notepad_plus_plus]"
+  "recipe[notepad_plus_plus]",
+  "recipe[oracle_java::win]"
