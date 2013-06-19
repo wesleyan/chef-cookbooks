7cb713d6a4f3fa9bd651bb0779bde7e260ca6144
Robert Christensen <rchristensen@wesleyan.edu>
2013-06-19 14:09:04 -0400

    Added libdvdcss to HandBrake

--- cookbooks/handbrake/recipes/mac.rb
+++ cookbooks/handbrake/recipes/mac.rb
@@ -6,6 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+# Install base Handbrake package.
 dmg_package "HandBrake" do
   app "HandBrake"
   volumes_dir "HandBrake-0.9.9-MacOSX.6_GUI_x86_64"
@@ -16,4 +17,17 @@ dmg_package "HandBrake" do
   type "app"
   package_id "fr.handbrake"
   version "0.9.9"
+end
+
+# As of VLC 2.0.x libdvdcss is packaged seperately, hence HandBrake needs it installed to decrypt DVDs.
+dmg_package "libdvdcss for HandBrake" do
+  app "libdvdcss"
+  volumes_dir "libdvdcss"
+  dmg_name "libdvdcss-1.2.12"
+  source "http://ims-chef.wesleyan.edu/os_x/handbrake/libdvdcss-1.2.12.dmg"
+  checksum "99406715de0cba91c836731fb9443d26d61736f4b9670963b8e2fd684c9f68ec"
+  action :install
+  type "pkg"
+  package_id "org.videolan.vlc.libdvdcss.2.pkg"
+  version "1.2.12"
 end
