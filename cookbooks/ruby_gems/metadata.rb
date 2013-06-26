b4e24078bd3fe2912c75dd7e976cc41239615957
Max Dietz <mdietz@wesleyan.edu>
2013-06-26 09:25:07 -0400

    Added new cookbook ruby_gems, responsible for installing any requested gems.  It just calls gem_package on them, so should the version of ruby be important, it will likely be better to start using the rvm cookbook.

---
+++ cookbooks/ruby_gems/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Wesleyan University"
+maintainer_email "software@wesleyan.edu"
+license          "All rights reserved"
+description      "Installs Ruby Gems"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
