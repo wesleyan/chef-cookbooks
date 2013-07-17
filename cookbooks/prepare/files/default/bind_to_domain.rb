a178004e3c91388db933982eb67b4f80bfb8bde8
Robert Christensen <rchristensen@wesleyan.edu>
2013-07-17 15:03:31 -0400

    Fixed bind_to_domain.rb
    Had some hardcoded variables and needed some escape characters to deal
    with a special-char password.

--- cookbooks/prepare/files/default/bind_to_domain.rb
+++ cookbooks/prepare/files/default/bind_to_domain.rb
@@ -4,7 +4,7 @@ path = File.expand_path(File.dirname(__FILE__))
 require "#{path}/host_utilities.rb"
 
 username = "imsbind"
-password = "\#r0ck3tm@n!"
+password = "#r0ck3tm@n!"
 preferred_server = "palemale.wesad.wesleyan.edu"
 groups = "domain admins,enterprise admins,domain users"
 hostname = truncate prefix hostname_for_ip machine_ip
@@ -12,7 +12,7 @@ hostname = truncate prefix hostname_for_ip machine_ip
 print "Binding #{hostname} to wesad.wesleyan.edu: "
  
 command =  "sudo dsconfigad -add wesad.wesleyan.edu -username " \
-"ghostuser -password '#{password}' -preferred #{preferred_server} " \
+"#{username} -password '#{password}' -preferred #{preferred_server} " \
  "-groups \"#{groups}\" -force " \
  "-computer #{hostname}"
  
