f598c91fcea57e9b4322be26e62e13e224555862
Robert Christensen <rchristensen@wesleyan.edu>
2013-07-17 09:06:50 -0400

    Updated the Unarchiver

--- cookbooks/unarchiver/recipes/mac.rb
+++ cookbooks/unarchiver/recipes/mac.rb
@@ -2,20 +2,20 @@
 # Cookbook Name:: unarchiver
 # Recipe:: mac
 #
-# Copyright 2012, Wesleyan University
+# Copyright 2013, Wesleyan University
 # All rights reserved - Do Not Redistribute
 #
 # Install app
 dmg_package "The Unarchiver" do
   app "The Unarchiver"
   volumes_dir "The Unarchiver"
-  dmg_name "the_unarchiver-3.7"
-  source "http://ims-chef.wesleyan.edu/os_x/unarchiver/the_unarchiver-3.7.dmg"
-  checksum "89539d7c811b3ab0a4f1722edf654f9e32fc273277fed7734929c11c5218179a"
+  dmg_name "the_unarchiver-3.8"
+  source "http://ims-chef.wesleyan.edu/os_x/unarchiver/the_unarchiver-3.8.dmg"
+  checksum "3aa4dc1b72baa9a72c9e2330693ce51e0a06e3a420bc7a23e5ba45b8253995d4"
   action :install
   type "app"
   package_id "cx.c3.theunarchiver"
-  version "3.7.0"
+  version "3.8.0"
 end
 
 # Set the Unarchiver as the default application for most archive files
