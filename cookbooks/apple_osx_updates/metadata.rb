e029028a1cffad1c466c4ca0262aa2f7ae7d9629
Robert Christensen <rchristensen@wesleyan.edu>
2013-07-17 08:45:58 -0400

    Updated OS X updates

--- cookbooks/apple_osx_updates/metadata.rb
+++ cookbooks/apple_osx_updates/metadata.rb
@@ -4,4 +4,4 @@ maintainer_email "software@wesleyan.edu"
 license          "All rights reserved"
 description      "Installs the latest OS X Update"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.3.0"
--- cookbooks/apple_osx_updates/recipes/mac.rb
+++ cookbooks/apple_osx_updates/recipes/mac.rb
@@ -19,3 +19,15 @@ dmg_package "OS X Update" do
   package_id "com.apple.pkg.update.os.10.8.4.12E55.delta"
   restart true
 end
+
+# Install current security update package.
+dmg_package "OS X Security Update" do
+  app "SecUpd2013-003"
+  volumes_dir "Security Update 2013-033"
+  dmg_name "SecUpd2013-003"
+  source "http://ims-chef.wesleyan.edu/os_x/apple_osx_updates/SecUpd2013-003.dmg"
+  checksum "4365a7d953e0c45c5e0eaa72f92f6a1239bc64d20f9d0931229bb934cbbb045a"
+  action :install
+  type "pkg"
+  package_id "com.apple.pkg.update.security.10.8.4.12E1009.2013.003"
+end
