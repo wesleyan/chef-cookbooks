01cc01543a020406b747fb1f909242004524a841
Robert Christensen <rchristensen@wesleyan.edu>
2013-07-17 09:38:41 -0400

    Fixed typo in Apple Airport

--- cookbooks/apple_airport/recipes/mac.rb
+++ cookbooks/apple_airport/recipes/mac.rb
@@ -10,7 +10,7 @@ dmg_package "Apple AirPort Utility" do
   app "AirPortUtility"
   volumes_dir "AirPortUtility"
   dmg_name "AirPortUtility6.3.1"
-  source "http://ims-chef.wesleyan.edu/os_x/apple_airport/AirPortUtility6.3.1dmg"
+  source "http://ims-chef.wesleyan.edu/os_x/apple_airport/AirPortUtility6.3.1.dmg"
   checksum "f0cfed9e0acadd22d5d1f642e8e6cf882697bc7760f588b4d35920af792bc2f4"
   action :install
   type "pkg"
