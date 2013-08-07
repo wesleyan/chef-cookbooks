a6a6929aa0a6c202b480f33be426d28319203450
Robert Christensen <rchristensen@wesleyan.edu>
2013-08-07 13:27:07 -0400

    Added MathWorks MATLAB 2013a

---
+++ cookbooks/mathworks_matlab/recipes/mac.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: mathworks_matlab
+# Recipe:: mac
+#
+# Copyright 2013, Wesleyan University
+#
+# All rights reserved - Do Not Redistribute
+#
+dmg_package "MathWorks MATLAB" do
+  app "MATLAB_R2013a"
+  volumes_dir "MATLAB"
+  dmg_name "MATLAB-2013a"
+  source "http://ims-chef.wesleyan.edu/os_x/mathworks_matlab/MATLAB-2013a.dmg"
+  checksum "37d8aca8a30619f09b66bcd1d6d4a925388c74b8a06287045e76ba84ca116e70"
+  action :install
+  type "app"
+  package_id "com.mathworks.matlab.r2013a"
+end
