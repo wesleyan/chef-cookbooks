8c5acdc3e7675b2de0627722b93039fa899c4c83
Robert Christensen <rchristensen@wesleyan.edu>
2013-08-13 16:47:58 -0400

    Switched EndNote to Site Installer
    ..still doesn't fucking work though.

--- cookbooks/thomson_reuters_endnote/recipes/mac.rb
+++ cookbooks/thomson_reuters_endnote/recipes/mac.rb
@@ -10,11 +10,11 @@
 dmg_package "Thomson Reuters EndNote" do
   app "EndNote X7"
   volumes_dir "EndNote X7 Installer"
-  dmg_name "EndNoteX7Installer"
-  source "http://ims-chef.wesleyan.edu/os_x/thomson_reuters_endnote/EndNoteX7Installer.dmg"
-  checksum "b071c542519a23362f6efa554cd47ab734ab8b1ef067ef061a95ecfde670bd51"
+  dmg_name "EndNoteX7SiteInstaller"
+  source "http://ims-chef.wesleyan.edu/os_x/thomson_reuters_endnote/EndNoteX7SiteInstaller.dmg"
+  checksum "ca4441db0535e768d4060fee5454463de60c773de141ebc31d2289882ca927d8"
   action :install
   type "dir"
-  package_id "com.endnote"
+  package_id "com.endnote.x7"
   version "17.0.0"
 end
