ac75bf5fad1a0d1a62fbb1b9312aeac272c201a1
Robert Christensen <rchristensen@wesleyan.edu>
2013-08-15 16:09:30 -0400

    Added Mplus to SS role

--- roles/osx-ss.rb
+++ roles/osx-ss.rb
@@ -4,6 +4,7 @@ description "This role is specific to machines require the software loadset for 
 run_list  "role[osx-classlab]",
           "recipe[gnu_r::mac]",
           "recipe[rstudio::mac]",
+          "recipe[muthen_mplus::mac]",
           "recipe[psyscope::mac]",
           "recipe[statacorp_stata::mac]",
           "recipe[ibm_spss::mac]"
