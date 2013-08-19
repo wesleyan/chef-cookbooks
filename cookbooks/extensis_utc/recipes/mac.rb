dadd4e086515a62bb3fa2cb848e02628bf00ff42
Robert Christensen <rchristensen@wesleyan.edu>
2013-08-19 17:16:37 -0400

    Updated UTC to 3.3.1

--- cookbooks/extensis_utc/recipes/mac.rb
+++ cookbooks/extensis_utc/recipes/mac.rb
@@ -10,12 +10,13 @@
 dmg_package "Universal Type Client" do
   app "Universal Type Client"
   volumes_dir "Universal Type Client"
-  dmg_name "universal_type_client-3.3.0"
-  source "http://ims-chef.wesleyan.edu/os_x/extensis_utc/universal_type_client-3.3.0.dmg"
-  checksum "7e325f7186de90dde22958a16aceca13f296be53f5aac32a88adf213c57c3fd5"
+  dmg_name "universal_type_client-3.3.1"
+  source "http://ims-chef.wesleyan.edu/os_x/extensis_utc/universal_type_client-3.3.1.dmg"
+  checksum "831659220543958f27470e8805f1605760d64dfd9ed23f2bfc6bd102e9309164"
   action :install
   type "pkg"
   package_id "com.extensis.UniversalTypeClient.universalTypeClient33.UniversalTypeClient.pkg"
+  version "3.3.1"
 end
 
 cookbook_file "/System/Library/User Template/English.lproj/Library/Preferences/com.extensis.UniversalTypeClient.plist" do
