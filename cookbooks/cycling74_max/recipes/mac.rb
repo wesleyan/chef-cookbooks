f2914ed867c15ebe1a1c60433f55c6db90d7c6da
Robert Christensen <rchristensen@wesleyan.edu>
2013-08-19 17:16:07 -0400

    Updated Max to 6.1.3
    Had to, 6.0 pkg had expired security cert.

--- cookbooks/cycling74_max/recipes/mac.rb
+++ cookbooks/cycling74_max/recipes/mac.rb
@@ -8,12 +8,12 @@
 #
 dmg_package "Cycling74 Max" do
   app "Max 6.1 Installer"
-  volumes_dir "Max61_130304_ee85fad3_keyserver"	
-  dmg_name "Max61_x86_ee85fad3_keyserver.dmg"
-  source "http://ims-chef.wesleyan.edu/os_x/cycling74_max/Max61_x86_ee85fad3_keyserver.dmg"
-  checksum "115f9c7be4c09248fecdd446b89458e891e1c91a39caef144327073f220d98ea"
+  volumes_dir "Max61_130620_69b6394_keyserver"	
+  dmg_name "Max613_69b6394_keyserver"
+  source "http://ims-chef.wesleyan.edu/os_x/cycling74_max/Max613_69b6394_keyserver.dmg "
+  checksum "d5fbcda041c5774da710b297ab936ab1f76458d3002e712e3ca73822d7c39e0f"
   action :install
   type "pkg"
   package_id "com.cycling74.Max61.pkg"
-  version "6.1.0"
+  version "6.1.3"
 end
