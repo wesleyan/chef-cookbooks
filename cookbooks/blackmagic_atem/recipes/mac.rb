92f4d46a8f3e5a1c5525fc802ba8e13170d59108
Robert Christensen <rchristensen@wesleyan.edu>
2013-08-20 11:54:54 -0400

    Updated Blackmagic ATEM

--- cookbooks/blackmagic_atem/recipes/mac.rb
+++ cookbooks/blackmagic_atem/recipes/mac.rb
@@ -8,13 +8,13 @@
 #
 # Install the package
 dmg_package "Blackmagic ATEM" do
-  app "Blackmagic ATEM Switchers Installer 4.0"
+  app "Blackmagic ATEM Switchers Installer 4.1.1"
   volumes_dir "Blackmagic ATEM Switchers"
-  dmg_name "Blackmagic_ATEM_Switchers_4.0"
-  source "http://ims-chef.wesleyan.edu/os_x/blackmagic_atem/Blackmagic_ATEM_Switchers_4.0.dmg"
-  checksum "e6e4dbb0d17282bf7831a4be5db450d5ef29fc082952744c5971cc2f6bf9ce50"
+  dmg_name "Blackmagic_ATEM_Switchers_4.1.1"
+  source "http://ims-chef.wesleyan.edu/os_x/blackmagic_atem/Blackmagic_ATEM_Switchers_4.1.1.dmg"
+  checksum "c4f39690bd2ee82fcf08df66f5573f42067b4118f965ffe52cf74a1db86a1ca4"
   action :install
   type "pkg"
   package_id "com.blackmagic-design.Switchers"
-  version "4.0.0"
+  version "4.1.1"
 end
