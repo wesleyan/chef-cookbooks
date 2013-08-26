7f854f13a0ab46c652514bce75fff760e74c2a1d
Max Dietz <maxwell.r.dietz@gmail.com>
2013-08-26 16:14:19 -0400

    now with settings!

--- cookbooks/apple_remote_desktop_admin/recipes/mac.rb
+++ cookbooks/apple_remote_desktop_admin/recipes/mac.rb
@@ -29,3 +29,15 @@ dmg_package "Apple Remote Desktop Admin Update" do
   type "pkg"
   package_id "com.apple.pkg.RemoteDesktopAdmin361"
 end
+
+dmg_package "Remote Desktop Settings" do
+  app "Remote Desktop Settings"
+  volumes_dir "Remote Desktop Settings"
+  dmg_name "Remote Desktop Settings"
+  source "http://ims-chef.wesleyan.edu/os_x/apple_remote_desktop_admin/Remote Desktop Settings.dmg"
+  checksum "fe481bbbffe197c16c86faac2023a1074976ed0b2a9f21d46e25c8edf7d556a1"
+  action :install
+  type "pkg"
+  version "1.0.0"
+  package_id "com.apple.pkg.settings.RemoteDesktopAdmin"
+end
