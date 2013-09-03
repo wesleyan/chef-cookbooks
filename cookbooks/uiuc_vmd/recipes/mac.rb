14cf79a856213ae59f73c6675d6e275e9a110f12
Robert Christensen <rchristensen@wesleyan.edu>
2013-09-03 09:34:23 -0400

    Add dock shortcut for VMD

--- cookbooks/uiuc_vmd/recipes/mac.rb
+++ cookbooks/uiuc_vmd/recipes/mac.rb
@@ -17,3 +17,18 @@ dmg_package "VMD" do
   package_id "edu.uiuc.ks.vmd"
   version "1.9.1"
 end
+
+# Create dock folder, if it does not exist
+dock_add "Natural Sciences and Mathematics" do
+  all_users true
+  action :folder_create
+  show_as "list"
+  display_as "folder"
+  arrangement "name"
+end
+
+# Add icon to dock
+dock_add "/Applications/VMD 1.9.1.app" do
+  all_users true
+  group "Natural Sciences and Mathematics"
+end
