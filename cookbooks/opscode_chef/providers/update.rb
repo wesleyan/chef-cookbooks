2164227db4f08db083dd3eb85b089d2368c7e82d
Max Dietz <maxwell.r.dietz@gmail.com>
2013-09-10 12:41:56 -0400

    fixed versioning and updated chef to 11.6.0

--- cookbooks/opscode_chef/providers/update.rb
+++ cookbooks/opscode_chef/providers/update.rb
@@ -1,7 +1,7 @@
 # Checks for a new chef version and updates
 action :update do
   require 'mixlib/versioning'
-  if not new_resource.version or Mixlib::Versioning.parse(Chef::VERSION) < Mixlib::Versioning.parse(new_resource.version)
+  if new_resource.version == nil or Mixlib::Versioning.parse(Chef::VERSION) < Mixlib::Versioning.parse(new_resource.version)
     system("/tmp/chef.sh")
   end
 end
