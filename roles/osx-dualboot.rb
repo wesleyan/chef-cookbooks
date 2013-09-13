1e0c2c8900cd41e452007931b5744b6cc3124627
Max Dietz <maxwell.r.dietz@gmail.com>
2013-09-13 11:25:40 -0400

    updated role

--- roles/osx-dualboot.rb
+++ roles/osx-dualboot.rb
@@ -1,3 +1,4 @@
 name "osx-dualboot"
 description "Installs BootChamp and rEFInd for dual boot machines"
-run_list "recipe[kainjow_bootchamp::mac]"
+run_list "recipe[kainjow_bootchamp::mac]",
+	 "recipe[refind::mac]"
