95ca304da2abfbe5607bc10c91bbc1911e2ee7c1
Robert Christensen <rchristensen@wesleyan.edu>
2013-09-13 16:42:43 -0400

    Updated this damned role again.

--- roles/osx-dualboot.rb
+++ roles/osx-dualboot.rb
@@ -5,4 +5,4 @@ description "Installs BootChamp and rEFInd for dual boot machines"
 run_list "role[osx-classlab]",
 		# These are dual-boot specific applications that make things easier for users.
 		 "recipe[kainjow_bootchamp::mac]",
-	 	 "recipe[refind::mac]"
+	 	 "recipe[rodsbooks_refind::mac]"
