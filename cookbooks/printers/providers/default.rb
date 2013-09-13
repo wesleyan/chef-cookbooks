eb0d26320246f274181ef65967cc19cf9c68567f
Max Dietz <maxwell.r.dietz@gmail.com>
2013-09-13 19:52:54 +0000

    added so removal continues on failure
    so that we can run it multiple times.

--- cookbooks/printers/providers/default.rb
+++ cookbooks/printers/providers/default.rb
@@ -7,14 +7,16 @@ action :add do
   if new_resource.ip
     system("lpadmin -p #{new_resource.name} -v lpd://#{new_resource.ip} -m \"#{new_resource.model}\" -D #{new_resource.name} -o printer-is-shared=false -E")
   else
-	  system("lpadmin -p #{new_resource.name} -v lpd://falcon.wesleyan.edu/#{new_resource.name} -m \"#{new_resource.model}\" -D #{new_resource.name} -E -o printer-is-shared=false -o HPOption_Duplexer=True -o Duplex=DuplexNoTumble -o Duplexer=Installed")
+    system("lpadmin -p #{new_resource.name} -v lpd://falcon.wesleyan.edu/#{new_resource.name} -m \"#{new_resource.model}\" -D #{new_resource.name} -E -o printer-is-shared=false -o HPOption_Duplexer=True -o Duplex=DuplexNoTumble -o Duplexer=Installed")
   end
 end
 
 action :remove do
-	system("lpadmin -x #{new_resource.name}")
+  execute "lpadmin -x #{new_resource.name}" do
+    returns [0,1]
+  end
 end
 
 action :set_default do
-	system("lpadmin -d #{new_resource.name}")
+  system("lpadmin -d #{new_resource.name}")
 end
