624a49eaf1b80405afc7d10510f7cc227ef2cecc
Max Dietz <maxwell.r.dietz@gmail.com>
2013-09-14 10:44:09 +0000

    fix reinstallation / reunzipping failures

--- cookbooks/rodsbooks_refind/recipes/mac.rb
+++ cookbooks/rodsbooks_refind/recipes/mac.rb
@@ -10,7 +10,11 @@
 cookbook_file "/tmp/refind-bin-0.7.4.zip"
 
 # Unzip the installer and run the install script.
-execute "unzip /tmp/refind-bin-0.7.4.zip -d /tmp/"
+execute "unzip /tmp/refind-bin-0.7.4.zip -d /tmp/" do
+  not_if { ::File.exists? "/tmp/refind-bin-0.7.4" }
+end
 
 # Run the install script.
-execute "/tmp/refind-bin-0.7.4/install.sh"
+execute "/tmp/refind-bin-0.7.4/install.sh && touch /var/db/receipts/com.rodsbooks.refind.plist" do
+  not_if { ::File.exists? "/var/db/receipts/com.rodsbooks.refind.plist"
+end
