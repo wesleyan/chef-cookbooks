356c87ed15cd3577821835844995aaa0ffda5834
Robert Christensen <rchristensen@wesleyan.edu>
2013-09-16 18:38:02 -0400

    Updated Office the correct way..

--- cookbooks/microsoft_office/metadata.rb
+++ cookbooks/microsoft_office/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "rchristensen@wesleyan.edu"
 license          "All rights reserved"
 description      "Installs Microsoft Office 2011 on OS X, 2013 on Windows"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.4.0"
+version          "0.5.0"
 depends			 "dmg"
 depends          "default_profile"
 depends          "dock"
--- cookbooks/microsoft_office/recipes/mac.rb
+++ cookbooks/microsoft_office/recipes/mac.rb
@@ -1,11 +1,11 @@
 #
-# Cookbook Name:: office
+# Cookbook Name:: microsoft_office
 # Recipe:: mac
 #
-# Copyright 2012, Wesleyan University
+# Copyright 2013, Wesleyan University
 #
 # All rights reserved - Do Not Redistribute
-
+#
 # Install Office base
 dmg_package "Microsoft Office 2011 SP3" do
   app "Microsoft Office 2011"
@@ -19,44 +19,8 @@ dmg_package "Microsoft Office 2011 SP3" do
   version "14.3.2"
 end
 
-#dmg_package "Update Office 2011-1434" do
-#  app "Office 2011 14.3.4 Update"
-#  volumes_dir "Microsoft Office 2011 14.3.4 Update"
-#  dmg_name "Office2011-1434UpdateEN"
-#  source "http://ims-chef.wesleyan.edu/os_x/microsoft_office_2011/Office2011-1434UpdateEN.dmg"
-#  checksum "892769fcdd91688ff66bddd9c5577e72cdd596420d8bd35f858a7e862f62f773"
-#  action :install
-#  type "pkg"
-#  package_id "com.microsoft.office.2011"
-#  version "14.3.4"
-#end
-
-#dmg_package "Update Office 2011-1435" do
-#  app "Office 2011 14.3.5 Update"
-#  volumes_dir "Microsoft Office 2011 14.3.5 Update"
-#  dmg_name "Office2011-1435UpdateEN"
-#  source "http://ims-chef.wesleyan.edu/os_x/microsoft_office_2011/Office2011-1435UpdateEN.dmg"
-#  checksum "6ddf86a7fd4cf3a3a5e0875ef5bc50178e2be0b691426e5eed997712bc9bb60e"
-#  action :install
-#  type "pkg"
-#  package_id "com.microsoft.office.2011"
-#  version "14.3.5"
-#end
-
-#dmg_package "Update Office 2011 Update" do
-#  app "Office 2011 14.3.6 Update"
-#  volumes_dir "Microsoft Office 2011 14.3.6 Update"
-#  dmg_name "Office2011-1436Update_EN-US"
-#  source "http://ims-chef.wesleyan.edu/os_x/microsoft_office_2011/Office2011-1436Update_EN-US.dmg"
-#  checksum "36dfc8abb3a4fd408d78910b308642482bdeb6035de88de63efdbd9e22e60e01"
-#  action :install
-#  type "pkg"
-#  package_id "com.microsoft.office.2011"
-#  version "14.3.6"
-#end
-
 # Install latest update.
-dmg_package "Update Office 2011 2011-1437" do
+dmg_package "Update Office 2011 2011" do
   app "Office 2011 14.3.7 Update"
   volumes_dir "Microsoft Office 2011 14.3.7 Update"
   dmg_name "Office2011-1437Update_EN-US"
