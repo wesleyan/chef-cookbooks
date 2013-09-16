b5a1c87d0268802323ed356dd727d8377a99b7d7
Max Dietz <maxwell.r.dietz@gmail.com>
2013-09-16 14:13:06 -0400

    added depends for office

--- cookbooks/microsoft_office/metadata.rb
+++ cookbooks/microsoft_office/metadata.rb
@@ -5,3 +5,6 @@ license          "All rights reserved"
 description      "Installs Microsoft Office 2011 on OS X, 2013 on Windows"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.4.0"
+depends			 "dmg"
+depends          "default_profile"
+depends          "dock"
