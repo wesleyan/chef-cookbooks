3e347509cf4d8a9b4873192741349c0b6a6b1f77
Max Dietz <maxwell.r.dietz@gmail.com>
2013-09-17 12:59:43 -0400

    added phonegap cookbook

---
+++ cookbooks/phonegap/CHANGELOG.md
@@ -0,0 +1,9 @@
+rodsbooks_refind CHANGELOG
+==========================
+
+This file is used to list changes made in each version of the phonegap cookbook.
+
+0.1.0
+-----
+- [mdietz] - Initial release of phonegap
+- [mdietz] - Current app version: 2.9.0
---
+++ cookbooks/phonegap/metadata.rb
@@ -0,0 +1,8 @@
+name             'phonegap'
+maintainer       'Wesleyan University'
+maintainer_email 'software@wesleyan.edu'
+license          'All rights reserved'
+description      'Installs/Configures Phonegap'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends          'nodejs'
