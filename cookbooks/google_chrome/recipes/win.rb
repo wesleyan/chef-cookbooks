dd35c387b222893b644ca64baa6daf10e461605c
Max Dietz <maxwell.r.dietz@gmail.com>
2013-09-19 13:46:18 -0400

    updated printers, updated google chrome windows

--- cookbooks/google_chrome/recipes/win.rb
+++ cookbooks/google_chrome/recipes/win.rb
@@ -1,8 +1,8 @@
 # Installs Chrome for Windows
 
 windows_package "Google Chrome" do
-  source "http://ims-chef.wesleyan.edu/windows/google_chrome/googlechrome-28.0.1500.71.msi"
-  version "28.0.150071"
+  source "http://ims-chef.wesleyan.edu/windows/google_chrome/googlechrome-29.0.1547.76.msi"
+  version "29.0.154776"
 end
 cookbook_file 'C:\bookmarks.html'
 cookbook_file 'C:\Program Files (x86)\Google\Chrome\Application\master_preferences'
