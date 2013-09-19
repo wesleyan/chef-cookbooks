89218e1b068fb63ebeb52e2f32c4917ffe24a59e
Robert Christensen <rchristensen@wesleyan.edu>
2013-09-19 09:52:00 -0400

    Fix ColorMunki typo

--- cookbooks/xrite_colormunki_photo/recipes/mac.rb
+++ cookbooks/xrite_colormunki_photo/recipes/mac.rb
@@ -10,7 +10,7 @@ dmg_package "X-Rite ColorMunki Photo" do
   app "ColormunkiPhoto"
   volumes_dir "ColormunkiPhoto-1.1.1"
   dmg_name "ColormunkiPhoto-1.1.1"
-  source "http://ims-chef.wesleyan.edu/os_x/colormunki_photo/ColormunkiPhoto-1.1.1.dmg"
+  source "http://ims-chef.wesleyan.edu/os_x/xrite_colormunki_photo/ColormunkiPhoto-1.1.1.dmg"
   checksum "1332ede34e7c804e5a21b3edeab5d6cb06c271db0f617582d022bd492fea7f01"
   action :install
   type "mpkg"
