df72c03f66cd9d53df8b04ddb87d02f95dd7ba22
Max Dietz <maxwell.r.dietz@gmail.com>
2013-10-02 14:26:16 -0400

    added version info

--- cookbooks/atlasti/recipes/win.rb
+++ cookbooks/atlasti/recipes/win.rb
@@ -10,4 +10,5 @@
 windows_package "AtlasTI" do
 	source "http://ims-chef.wesleyan.edu/windows/atlasti/a7setup.msi"
 	checksum "b97d21eb5210f11bcb887532cf326bcd5ab044de13393fc33c1f1301fac54493"
+	version "7.0.0"
 end
