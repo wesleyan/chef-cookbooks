f67469312eef8c32b7353a61626754a7c662a554
Max Dietz <maxwell.r.dietz@gmail.com>
2013-10-17 13:42:00 -0400

    fixed switches names

--- cookbooks/autodesk_maya/metadata.rb
+++ cookbooks/autodesk_maya/metadata.rb
@@ -1,7 +1,7 @@
-name             '9bis_kitty'
+name             'autodesk_maya'
 maintainer       'Wesleyan University'
 maintainer_email 'software@wesleyan.edu'
 license          'All rights reserved'
-description      'Installs/Configures 9bis_kitty'
+description      'Installs/Configures autodesk_maya'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
