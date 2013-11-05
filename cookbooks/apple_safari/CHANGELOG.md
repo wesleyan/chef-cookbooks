2e920d20787eb68dab65c2717986156b08383f24
Robert Christensen <rchristensen@wesleyan.edu>
2013-11-05 12:22:52 -0500

    Updated Apple Safari

--- cookbooks/apple_safari/CHANGELOG.md
+++ cookbooks/apple_safari/CHANGELOG.md
@@ -1,12 +1,20 @@
-# CHANGELOG for safari
+apple_safari CHANGELOG
+======================
 
-This file is used to list changes made in each version of safari.
+This file is used to list changes made in each version of apple_safari
 
-## 0.2.0:
+0.3.0
+-----
+- [rchristensen] - Updated app to 6.1
+- [rchristensen] - Brought README/metadata/CHANGELOG up to code.
+- [mdietz] - Fixed bookmarks.
+- [mdietz] - Fixed Java plist issue.
 
-* Updated app to 6.0.4
+0.2.0
+-----
+- [rchristensen] - Updated app to 6.0.4
 
-## 0.1.0:
-
-* Initial release of safari
-* Current app version is 6.0.2
+0.1.0
+-----
+- [rchristensen] - Initial release of safari
+- [rchristensen] - Current app version is 6.0.2
--- cookbooks/apple_safari/recipes/mac.rb
+++ cookbooks/apple_safari/recipes/mac.rb
@@ -2,32 +2,34 @@
 # Cookbook Name:: safari
 # Recipe:: mac
 #
-# Copyright 2012, Wesleyan University
+# Copyright 2013, Wesleyan University
 #
 # All rights reserved - Do Not Redistribute
 #
-# Comment out actual install for now as 10.8.4 updater includes Safari 6.0.5
-#dmg_package "Update Safari" do
-# app "Safari6.0.4Mountain"
-# volumes_dir "Safari"
-# dmg_name "safari-6.0.4"
-# source "http://ims-chef.wesleyan.edu/os_x/apple_safari/safari-6.0.4.dmg"
-# checksum "aea490ab7a4d6bfdaaf07b826c8e8e4469dc8ad6f9e826ad19b2e458478e71df"
-# action :install
-# type "pkg"
-# package_id "com.apple.pkg.Safari6.0.4Mountain"
-#end
+# Install our pkg
+dmg_package "Update Safari" do
+ app "Safari6.1MountainLion"
+ volumes_dir "Safari"
+ dmg_name "safari-6.1"
+ source "http://ims-chef.wesleyan.edu/os_x/apple_safari/safari-6.1.dmg"
+ checksum "a8fb24963ff844deca0ef9f467b852fe2c09d51ccabf535d6a2ac3388d5dbe46"
+ action :install
+ type "pkg"
+ package_id "com.apple.pkg.Safari6.1MountainLion"
+end
 
 # Add to dock
 dock_add "/Applications/Safari.app" do
   all_users true
 end
 
+# Seed plist file to enable Java/suppress Pop-ups.
 default_profile "com.apple.Safari.plist" do
   path "Library/Preferences"
   cookbook 'apple_safari'
 end
 
+# Seed Bookmarks file.
 default_profile "Bookmarks.plist" do
   path "Library/Safari"
   cookbook 'apple_safari'
