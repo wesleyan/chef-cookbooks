b6c72f69dc6d410acf4325686a9cf6cb38bab02f
Robert Christensen <rchristensen@wesleyan.edu>
2013-11-05 14:51:36 -0500

    Updated to production env

--- environments/production.rb
+++ environments/production.rb
@@ -1,7 +1,9 @@
 name "production"
 description "This is our production environment, it is for recipes and cookbooks that have been thoroughly tested for bugs and regressions"
-cookbook_versions 	"apple_itunes" => "<= 0.4.1",
-				    "apple_safari" => "<= 0.2.0",
-				    "eset_nod32" => "<= 0.1.0",
-					"mozilla_firefox" => "<= 0.4.7",
-					"opscode_chef" => "<= 0.2.0"
+cookbook_versions 	"apple_itunes" => "<= 0.4.2",
+					"apple_remote_desktop_client" => "<= 0.1.0",
+					"apple_remote_desktop_admin" => "<= 0.2.0",
+				    "apple_safari" => "<= 0.3.0",
+				    "eset_nod32" => "<= 0.2.0",
+					"mozilla_firefox" => "<= 0.4.8",
+					"opscode_chef" => "<= 0.3.0"
