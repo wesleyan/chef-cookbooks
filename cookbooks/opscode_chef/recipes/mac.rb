0b6f2173be7820076c4f193b15a46b80199abf5b
Max Dietz <maxwell.r.dietz@gmail.com>
2013-11-05 13:14:23 -0500

    updated opscode_chef to 11.8

--- cookbooks/opscode_chef/recipes/mac.rb
+++ cookbooks/opscode_chef/recipes/mac.rb
@@ -11,11 +11,11 @@ chef_gem "mixlib-versioning"
 
 # Get chef install script from our server
 remote_file "/tmp/chef.sh" do
-  source "http://ims-chef.wesleyan.edu/os_x/opscode_chef/chef-11.6.0_1.mac_os_x.10.7.2.sh"
+  source "http://ims-chef.wesleyan.edu/os_x/opscode_chef/chef-11.8.0_1.mac_os_x.10.7.2.sh"
   mode 0777
 end
 
 # Run shell script to update chef-client
 opscode_chef_update "Updating version" do
-  version "11.6.0"
+  version "11.8.0"
 end
