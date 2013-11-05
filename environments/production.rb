385f4b5791869c309018b4b944b8fd036097ed65
Max Dietz <maxwell.r.dietz@gmail.com>
2013-11-05 13:26:42 -0500

    added environment file for production

---
+++ environments/production.rb
@@ -0,0 +1,4 @@
+name "production"
+description "This is our production environment, it is for recipes and cookbooks that have been thoroughly tested for bugs and regressions"
+cookbook_versions 	"apple_itunes" => "<= 0.4.1",
+					"opscode_chef" => "<= 0.2.0"
