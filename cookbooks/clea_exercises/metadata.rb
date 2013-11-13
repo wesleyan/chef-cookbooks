0f28df3ee860b76ee5f53e505782e2c080570ec7
Max Dietz <mdietz@wesleyan.edu>
2013-11-13 14:25:08 -0500

    clea cookbook added

---
+++ cookbooks/clea_exercises/metadata.rb
@@ -0,0 +1,7 @@
+name             'clea_exercises'
+maintainer       'Wesleyan University'
+maintainer_email 'software@wesleyan.edu'
+license          'All rights reserved'
+description      'Installs/Configures clea_exercises'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
