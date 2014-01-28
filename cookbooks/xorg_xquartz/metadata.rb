e6b226f831e7e2775e23fa6c0f3da0b883788c54
Robert Christensen <rchristensen@wesleyan.edu>
2014-01-28 17:22:11 -0500

    Updated XQuartz

--- cookbooks/xorg_xquartz/metadata.rb
+++ cookbooks/xorg_xquartz/metadata.rb
@@ -1,6 +1,8 @@
-maintainer       "Wesleyan University"
-maintainer_email "rchristensen@wesleyan.edu"
-license          "All rights reserved"
-description      "Installs XQuartz"
+name             'xorg_xquartz'
+maintainer       'Wesleyan University'
+maintainer_email 'software@wesleyan.edu'
+license          'All rights reserved'
+description      'Installs XQuartz'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          '0.2.0'
+depends          'dmg'
--- cookbooks/xorg_xquartz/recipes/mac.rb
+++ cookbooks/xorg_xquartz/recipes/mac.rb
@@ -2,22 +2,25 @@
 # Cookbook Name:: xorg_xquartz
 # Recipe:: mac
 #
-# Copyright 2013, Wesleyan University
+# Copyright 2014, Wesleyan University
 #
 # All rights reserved - Do Not Redistribute
 #
-dmg_package "XQuartz-2.7.4.dmg" do
+# Install our pkg
+dmg_package "XQuartz-2.7.5.dmg" do
   app "XQuartz"
-  volumes_dir "XQuartz-2.7.4"
-  dmg_name "XQuartz-2.7.4"
-  source "http://ims-chef.wesleyan.edu/os_x/xorg_xquartz/XQuartz-2.7.4.dmg"
-  checksum "3f7c156fc4b13e3f0d0e44523ef2bd3cf7ea736126616dd2da28abb31840923c"
+  volumes_dir "XQuartz-2.7.5"
+  dmg_name "XQuartz-2.7.5"
+  source "http://ims-chef.wesleyan.edu/os_x/xorg_xquartz/XQuartz-2.7.5.dmg"
+  checksum "4382ff78cef5630fb6b8cc982da2e5a577d8cc5dddd35a493b50bad2fcf5e34a"
   action :install
   type "pkg"
   package_id "org.macosforge.xquartz.pkg"
+  version "2.7.5"
 end
 
-execute "symlink x11" do
+# Here we symlink the default X11.app with XQuartz.app
+execute "Symlink X11" do
   command "ln -s /Applications/Utilities/XQuartz.app /Applications/Utilities/X11.app"
   not_if { File.exist?("/Applications/Utilities/X11.app") }
 end
