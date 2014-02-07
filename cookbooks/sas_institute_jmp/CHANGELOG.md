cfc53034be27bf4dc92b7beaec86e8c0e5fb3344
Robert Christensen <rchristensen@wesleyan.edu>
2014-02-07 09:50:52 -0500

    Renamed SAS JMP and updated recipes to 11.0.x

---
+++ cookbooks/sas_institute_jmp/CHANGELOG.md
@@ -0,0 +1,16 @@
+sas_institute_jmp CHANGELOG
+===========================
+
+This file is used to list changes made in each version of the sas_institute_jmp cookbook.
+
+0.2.0
+-----
+- [rchristensen] - Updated app to 11.0 on OS X and Windows.
+- [rchristensen] - Updated licensing files on OS X.
+- [rchristensen] - Brought CHANGELOG/metadata/README up to code.
+- [mdietz] - Added Windows recipe.
+
+0.1.0
+-----
+- [rchristensen] - Initial release of sas_jmp
+- [rchristensen] - Current app version: 10.0
--- cookbooks/sas_institute_jmp/recipes/win.rb
+++ cookbooks/sas_institute_jmp/recipes/win.rb
@@ -1,33 +1,35 @@
 #
-# Cookbook Name:: sas_jmp
+# Cookbook Name:: sas_institute_jmp
 # Recipe:: win
 #
-# Copyright 2013, Wesleyan University
+# Copyright 2014, Wesleyan University
 #
 # All rights reserved - Do Not Redistribute
 #
-
+# Pull down our installer
 remote_file "Download SAS JMP" do
-  source "http://ims-chef.wesleyan.edu/windows/sas_jmp/JMP.zip"
-  path 'C:\TEMP\JMP.zip'
-  checksum "cf5f8048e6367d980f8ca29adccef4f718e7a1be28d00c3e6053229bffa02dca"
+  source "http://ims-chef.wesleyan.edu/windows/sas_institute_jmp/JMP-11.zip"
+  path 'C:\TEMP\JMP-11.zip'
+  checksum "023d216333c62492d9df1eef17041b7a6fae057d6aded19b3c2c65f6fda04bae"
   backup false
 end
 
-execute "expand jmp" do
+# Extract our installer
+execute "Extract JMP" do
   command "\"C:\\Program Files (x86)\\7-Zip\\7z.exe\" x \"C:\\TEMP\\JMP.zip\" -y -o\"C:\\TEMP"
   action :run
 end
 
+# Install!
 windows_package "SAS JMP" do
   source 'C:\TEMP\JMP\setup.exe'
   installer_type :custom
   options '/s /v"qn"'
-  version "10.0.0"
+  version "11.0.0"
 end
 
 # Insert JMP license file
 cookbook_file 'winJMP.per' do
-  path 'C:\Program Files\SAS\JMP\10\JMP.per'
+  path 'C:\Program Files\SAS\JMP\11\JMP.per'
   backup false
 end
