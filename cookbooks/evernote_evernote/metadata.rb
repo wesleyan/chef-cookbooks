de8d0b89d94b9f79cc95c0457a2087449ebd6fc6
Robert Christensen <rchristensen@wesleyan.edu>
2014-02-10 15:27:48 -0500

    Removed Evernote

--- cookbooks/evernote_evernote/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'evernote_evernote'
-maintainer       'Wesleyan University'
-maintainer_email 'software@wesleyan.edu'
-license          'All rights reserved'
-description      'Installs/Configures Evernote software'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
