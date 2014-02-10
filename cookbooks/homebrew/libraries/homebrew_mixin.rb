3121443b110d93223c17ba318bb515f14521b6a3
Robert Christensen <rchristensen@wesleyan.edu>
2014-02-10 15:07:52 -0500

    updated homebrew to 1.5.4

--- cookbooks/homebrew/libraries/homebrew_mixin.rb
+++ cookbooks/homebrew/libraries/homebrew_mixin.rb
@@ -2,7 +2,7 @@
 # Author:: Joshua Timberman (<jtimberman@opscode.com>)
 # Author:: Graeme Mathieson (<mathie@woss.name>)
 # Cookbook Name:: homebrew
-# Attributes:: default
+# Libraries:: homebrew_mixin
 #
 # Copyright 2011-2013, Opscode, Inc.
 #
@@ -19,4 +19,35 @@
 # limitations under the License.
 #
 
-default['homebrew']['owner'] = "administrator"
+module Homebrew
+  # Homebrew
+  module Mixin
+    def homebrew_owner
+      @homebrew_owner ||= calculate_owner
+    end
+
+    private
+
+    def calculate_owner
+      owner = homebrew_owner_attr || sudo_user || current_user
+      if owner == 'root'
+        fail Chef::Exceptions::User,
+             "Homebrew owner is 'root' which is not supported. " +
+             "To set an explicit owner, please set node['homebrew']['owner']."
+      end
+      owner
+    end
+
+    def homebrew_owner_attr
+      node['homebrew']['owner']
+    end
+
+    def sudo_user
+      ENV['SUDO_USER']
+    end
+
+    def current_user
+      ENV['USER']
+    end
+  end
+end
