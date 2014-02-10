c1406f0e7b24791d7b76b86ab953dee6874586a7
Robert Christensen <rchristensen@wesleyan.edu>
2014-02-10 16:45:16 -0500

    Turned on a bunch of shit in production..

--- environments/production.rb
+++ environments/production.rb
@@ -1,15 +1,14 @@
 name "production"
 description "This is our production environment, it is for recipes and cookbooks that have been thoroughly tested for bugs and regressions"
-cookbook_versions 	"apple_itunes" => "<= 0.4.3",
-					"apple_remote_desktop_client" => "<= 0.1.0",
+cookbook_versions 	"apple_itunes" => "<= 0.4.4",
+					"apple_remote_desktop_client" => "<= 0.2.0",
 					"apple_remote_desktop_admin" => "<= 0.2.0",
-				    "apple_safari" => "<= 0.3.0",
-				    "apple_xcode" => "<= 0.4.0",
-				    "chef_chefclient" => "<= 0.3.0",
+				    "apple_safari" => "<= 0.4.0",
+				    "apple_xcode" => "<= 0.5.0",
+				    "chef_chefclient" => "<= 0.4.0",
 				    "eset_nod32" => "<= 0.2.0",
 				    "homebrew" => "<= 1.3.0",
-				    "mathworks_matlab" => "<= 0.1.0",
+				    "mathworks_matlab" => "<= 0.2.0",
 					"mozilla_firefox" => "<= 0.4.9",
-					"opscode_chef" => "<= 0.3.0",
 					"prepare" => "<= 0.0.0",
-					"xorg_xquartz" => "<= 0.1.0"
+					"xorg_xquartz" => "<= 0.2.0"
