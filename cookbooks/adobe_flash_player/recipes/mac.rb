f9ed6a26cbcdfcf45d33192909fc4138648ad0de
Robert Christensen <rchristensen@wesleyan.edu>
2014-02-11 12:17:38 -0500

    Updated Adobe Flash Player

--- cookbooks/adobe_flash_player/recipes/mac.rb
+++ cookbooks/adobe_flash_player/recipes/mac.rb
@@ -2,21 +2,21 @@
 # Cookbook Name:: adobe_flash_player
 # Recipe:: mac
 #
-# Copyright 2013, Wesleyan University
+# Copyright 2014, Wesleyan University
 #
 # All rights reserved - Do Not Redistribute
 #
 # Install our package, extracted from Adobe's custom installer .app.
 dmg_package "Adobe Flash Player" do
-  app "Install Adobe Flash Player"
-  volumes_dir "Flash Player"
-  dmg_name "adobe_flash_player-11.9.900.117"
-  source "http://ims-chef.wesleyan.edu/os_x/adobe_flash_player/adobe_flash_player-11.9.900.117.dmg"
-  checksum "92e1885970522da6b9fd67b60aa55406c1e3f0eae20ed0550614d02e0fa671be"
+  app "AdobeFlashPlayer"
+  volumes_dir "AdobeFlashPlayer"
+  dmg_name "adobe_flash_player-12.0.0.4"
+  source "http://ims-chef.wesleyan.edu/os_x/adobe_flash_player/adobe_flash_player-12.0.0.4.dmg"
+  checksum "3dc57ac8a66a6eda285d82e45d0083bf5240f2db2e852b64e465cfc663993822"
   action :install
   type "pkg"
   package_id "com.adobe.pkg.FlashPlayer"
-  version "11.9.900117"
+  version "12.0.04"
 end
 
 # Create directory for mms.cfg
