95e64d909c84c0df19fc751c0a9f77c1b7b2432c
Robert Christensen <rchristensen@wesleyan.edu>
2014-02-12 15:10:25 -0500

    Removed unneeded ruby gems recipe

--- roles/osx-cfa.rb
+++ roles/osx-cfa.rb
@@ -17,5 +17,4 @@ run_list  "role[osx-classlab]",
           "recipe[troikatronix_isadora::mac]",
           "recipe[quark_quarkxpress::mac]",
           "recipe[extensis_utc::mac]",
-          "recipe[nemetschek_vectorworks::mac]",
-          "recipe[ruby_gems]"
+          "recipe[nemetschek_vectorworks::mac]"
