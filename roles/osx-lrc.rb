7eaf01e3600e2d10662e24382e827bcbd08ed01b
Robert Christensen <rchristensen@wesleyan.edu>
2014-02-14 15:28:36 -0500

    Added Dreamweaver to LRC role

--- roles/osx-lrc.rb
+++ roles/osx-lrc.rb
@@ -5,4 +5,5 @@ run_list  "role[osx-classlab]",
           "recipe[audacity::mac]",
           "recipe[aegisub::mac]",
           "recipe[apple_quicktime_pro::mac]",
-          "recipe[roxio_toast_titanium::mac]"
+          "recipe[roxio_toast_titanium::mac]",
+          "recipe[adobe_dreamweaver_cs6::mac]"
