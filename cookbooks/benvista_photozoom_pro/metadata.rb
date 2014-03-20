ff8899b3611bceeef184331cd085166129156ac0
Robert Christensen <rchristensen@wesleyan.edu>
2014-03-19 21:18:09 -0400

    Added PhotoZoom Pro

---
+++ cookbooks/benvista_photozoom_pro/metadata.rb
@@ -0,0 +1,8 @@
+name             'benvista_photozoom_pro'
+maintainer       'Wesleyan University'
+maintainer_email 'software@wesleyan.edu'
+license          'All rights reserved'
+description      'Installs BenVista PhotoZoom Pro'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends          'dmg'
