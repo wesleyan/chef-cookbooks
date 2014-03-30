82958e27990767196ec66fbf9cc3ded09e0bcbf8
Robert Christensen <rchristensen@wesleyan.edu>
2014-03-30 19:47:29 -0400

    URL fix :(

--- cookbooks/kintekcorp_kintek_explorer/recipes/mac.rb
+++ cookbooks/kintekcorp_kintek_explorer/recipes/mac.rb
@@ -11,7 +11,7 @@ dmg_package "KinTek Explorer" do
   app "KinTek_Explorer"
   volumes_dir "KinTek_Explorer"
   dmg_name "kintek_explorer-student-4.0.131124"
-  source "http://ims-chef.wesleyan.edu/os_x/kintekcorp_kintek_explorer/kintek_explorer-student-4.0.11242013.dmg"
+  source "http://ims-chef.wesleyan.edu/os_x/kintekcorp_kintek_explorer/kintek_explorer-student-4.0.131124.dmg"
   checksum "2b7cf661e8a8ff9dcc5110735e95b895cd4c9c34aea3939676c34b992c5ecae0"
   action :install
   type "dir"
