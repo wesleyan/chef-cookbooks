6b803222673824176d79a20fe9dd7780af0d3f1f
Robert Christensen <rchristensen@wesleyan.edu>
2014-06-03 08:58:14 -0400

    Removed QuarkXpress
    EOL’d by ITS, didn’t work well anyway.

--- cookbooks/quark_quarkxpress/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'quark_quarkxpress'
-maintainer       'Wesleyan University'
-maintainer_email 'software@wesleyan.edu'
-license          'All rights reserved'
-description      'Installs QuarkXPress'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
-depends			 'dmg'
