478f789c38b93789ce56b5483044b1f45e81880d
Robert Christensen <rchristensen@wesleyan.edu>
2014-06-04 12:50:34 -0400

    Re-added common dependency to launch_assoc

--- cookbooks/launch_association/metadata.rb
+++ cookbooks/launch_association/metadata.rb
@@ -4,4 +4,5 @@ license          'All rights reserved'
 description      'Manages file type associations'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
+depends			 'common'
 depends          'homebrew'
