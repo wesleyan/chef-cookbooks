6c629c64793ce203d97d96e54e1d3c3a1f92cfeb
Robert Christensen <rchristensen@wesleyan.edu>
2014-06-06 10:07:44 -0400

    Fixed launch_association
    Now accounts for UID’s of system users created by AutoDMG.. a bit hacky
    but I don’t have time for a better solution.

--- cookbooks/launch_association/providers/set_file_handler.rb
+++ cookbooks/launch_association/providers/set_file_handler.rb
@@ -2,7 +2,7 @@ action :set_file_handler do
   uti = new_resource.uti ? new_resource.uti : new_resource.name
   if(new_resource.all_users)
    for cur_user in get_users_list
-    if cur_user.uid >= 500 && ::File.directory?(cur_user.user_dir)
+    if cur_user.uid >= 498 && ::File.directory?(cur_user.user_dir)
       final_user = cur_user
       execute "duti -s #{new_resource.bundle_id} #{uti} #{new_resource.role}" do
         command "duti -s #{new_resource.bundle_id} #{uti} #{new_resource.role}"
@@ -13,7 +13,7 @@ action :set_file_handler do
    end
    copy_plist_for_user_template("#{final_user.user_dir}/Library/Preferences/com.apple.LaunchServices.plist")
   else
-   if(current_user.uid >= 500 && ::File.directory?(current_user.user_dir))
+   if(current_user.uid >= 498 && ::File.directory?(current_user.user_dir))
      execute "duti -s #{new_resource.bundle_id} #{uti} #{new_resource.role}" do
        command "duti -s #{new_resource.bundle_id} #{uti} #{new_resource.role}"
        user current_user.username
--- cookbooks/launch_association/providers/set_url_handler.rb
+++ cookbooks/launch_association/providers/set_url_handler.rb
@@ -1,7 +1,7 @@
 action :set_url_handler do
   if (new_resource.all_users)
     for cur_user in get_users_list
-      if cur_user.uid >= 500 && ::File.directory?(cur_user.user_dir)
+      if cur_user.uid >= 498 && ::File.directory?(cur_user.user_dir)
         final_user = cur_user
         execute "duti -s #{new_resource.bundle_id} #{new_resource.uti}" do
           user cur_user.username
@@ -10,7 +10,7 @@ action :set_url_handler do
       end
     end
     copy_plist_for_user_template("#{final_user.user_dir}/Library/Preferences/com.apple.LaunchServices.plist")
-  elsif current_user.uid >= 500 and ::File.directory?(current_user.user_dir)
+  elsif current_user.uid >= 498 and ::File.directory?(current_user.user_dir)
     execute "duti -s #{new_resource.bundle_id} #{new_resource.uti}" do
       user current_user.username
       returns [0,1,2]
