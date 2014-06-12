d7c3a1cbca657ef476aba976b36d9dcd6b2ddfd2
Robert Christensen <rchristensen@wesleyan.edu>
2014-06-11 22:49:30 -0400

    Updated MacVim

--- cookbooks/macvim/README.md
+++ cookbooks/macvim/README.md
@@ -1,14 +1,40 @@
-Description
-===========
-Installs MacVim on OS X
+macvim Cookbook
+===============
+This cookbook installs MacVim, a text editor.
 
 Requirements
-============
-10.8+
+------------
+#### platform
+- `OS X` - 10.9 or above.
 
 Attributes
-==========
+----------
 
 Usage
-=====
-Use the cookbook!
+-----
+#### macvim::mac
+
+Just include `macvim::mac` in your node's `run_list`:
+
+```json
+{
+  "name":"my_node",
+  "run_list": [
+    "recipe[macvim::mac]"
+  ]
+}
+```
+
+Contributing
+------------
+
+1. Fork the repository on Github
+2. Create a named feature branch (like `add_component_x`)
+3. Write you change
+4. Write tests for your change (if applicable)
+5. Run the tests, ensuring they all pass
+6. Submit a Pull Request using Github
+
+License and Authors
+-------------------
+Robert Christensen <rchristensen@wesleyan.edu>
--- cookbooks/macvim/recipes/mac.rb
+++ cookbooks/macvim/recipes/mac.rb
@@ -2,31 +2,35 @@
 # Cookbook Name:: macvim
 # Recipe:: mac
 #
-# Copyright 2012, Wesleyan University
+# Copyright 2014, Wesleyan University
 #
 # All rights reserved - Do Not Redistribute
 #
-dmg_package "MacVim-7.3-snapshot-66.dmg" do
-  app "MacVim"
-  volumes_dir "MacVim"
-  dmg_name "MacVim-7.3-snapshot-66"
-  source "http://ims-chef.wesleyan.edu/os_x/macvim/MacVim-7.3-snapshot-66.dmg"
-  checksum "c0422e98ee9fafd9c6dca05ee250acd4bdec1e8dfcfb012e88a4700483cddde1"
+# Install our package.
+dmg_package 'MacVim-7.4-snapshot-73.dmg' do
+  app 'MacVim'
+  volumes_dir 'MacVim'
+  dmg_name 'MacVim-7.4-snapshot-73'
+  source 'http://sw.wesleyan.edu/os_x/macvim/MacVim-7.4-snapshot-73.dmg'
+  checksum 'c0422e98ee9fafd9c6dca05ee250acd4bdec1e8dfcfb012e88a4700483cddde1'
   action :install
-  type "app"
+  type 'app'
 end
 
+# Install the mvim script so that users can run MacVim from the command line.
+cookbook_file '/usr/local/bin/mvim'
+
 # Create dock folder, if it does not exist
-dock_add "Natural Sciences and Mathematics" do
+dock_add 'Natural Sciences and Mathematics' do
   all_users true
   action :folder_create
-  show_as "list"
-  display_as "folder"
-  arrangement "name"
+  show_as 'list'
+  display_as 'folder'
+  arrangement 'name'
 end
 
 # Add icon to dock
-dock_add "/Applications/MacVim.app" do
+dock_add '/Applications/MacVim.app' do
   all_users true
-  group "Natural Sciences and Mathematics"
+  group 'Natural Sciences and Mathematics'
 end
