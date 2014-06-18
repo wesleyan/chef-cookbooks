eb29dfb72ac73d0fd2faa01d633217f92fd53361
Robert Christensen <rchristensen@wesleyan.edu>
2014-06-18 15:37:54 -0400

    ccp4 metadata

---
+++ cookbooks/ccp4/metadata.rb
@@ -0,0 +1,10 @@
+name			       'ccp4'
+maintainer       'Wesleyan University'
+maintainer_email 'software@wesleyan.edu'
+license          'Apache 2.0'
+description      'Installs CCP4 and COOT'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.2.0'
+depends			     'dmg'
+depends			     'dock'
+depends			     'xorg_xquartz'
