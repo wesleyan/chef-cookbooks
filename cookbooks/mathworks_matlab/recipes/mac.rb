d675facbeb4826ccfae0d4e2affc45ee1781a05a
Robert Christensen <rchristensen@wesleyan.edu>
2014-06-18 15:48:53 -0400

    Added Apache header to a lot of recipes.

--- cookbooks/mathworks_matlab/recipes/mac.rb
+++ cookbooks/mathworks_matlab/recipes/mac.rb
@@ -2,9 +2,17 @@
 # Cookbook Name:: mathworks_matlab
 # Recipe:: mac
 #
-# Copyright 2014, Wesleyan University
-#
-# All rights reserved - Do Not Redistribute
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 # Remove MATLAB 2013a if it's installed
 directory "/Applications/MATLAB_R2013a" do
