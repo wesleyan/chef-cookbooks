c52d008f176a6293d463e905620b8d9207d3e19a
Robert Christensen <rchristensen@wesleyan.edu>
2014-08-06 10:51:32 -0400

    Updated MATLAB

--- cookbooks/mathworks_matlab/recipes/mac.rb
+++ cookbooks/mathworks_matlab/recipes/mac.rb
@@ -15,22 +15,22 @@
 # limitations under the License.
 #
 # Remove MATLAB 2013a if it's installed
-directory "/Applications/MATLAB_R2013a" do
-  recursive true
-  action :delete
-end
+#directory "/Applications/MATLAB_R2013a" do
+#  recursive true
+#  action :delete
+#end
 
 # Install our .app, built with the MATLAB installer.
 dmg_package "MathWorks MATLAB" do
-  app "MATLAB_R2013b"
+  app "MATLAB_R2014a"
   volumes_dir "MATLAB"
-  dmg_name "MATLAB-2013b"
-  source "http://ims-chef.wesleyan.edu/os_x/mathworks_matlab/MATLAB-2013b-labclass.dmg"
-  checksum "04010c7aa7d6f0308347e631acf0c6df049d4d17c21689c2ed83beb9e2fe4f86"
+  dmg_name "MATLAB-2014a-labclass"
+  source "http://ims-chef.wesleyan.edu/os_x/mathworks_matlab/MATLAB-2014a-labclass.dmg"
+  checksum "4809b95f0d4e5230ec7750543e31a6ba6e1acfc25f250bc1b92839ce0daa1060"
   action :install
   type "app"
   package_id "com.mathworks.matlab"
-  version "2013.2.0"
+  version "2014.1.0"
 end
 
 # Create dock folder, if it does not exist
@@ -43,7 +43,7 @@ dock_add "Natural Sciences and Mathematics" do
 end
 
 # Add icon to dock
-dock_add "/Applications/MATLAB_R2013b.app" do
+dock_add "/Applications/MATLAB_R2014a.app" do
   all_users true
   group "Natural Sciences and Mathematics"
 end
